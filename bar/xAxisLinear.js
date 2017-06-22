ff288888a7a14724fc144c25aeda47b623b87d80
Bob Haslett <bob.haslett@ft.com>
2017-06-22 21:06:56 +0100

    add tick alignment on y-axis

--- bar/xAxisLinear.js
+++ bar/xAxisLinear.js
@@ -5,57 +5,69 @@ function xAxisLinear() {
     let tickSize = 50;
     let offset = 0;
     let numTicks = 5
+    let align = "bottom"
+    let xLabel
 
     function axis(parent) {
 
-        const xAxis =d3.axisBottom()
+        const xAxis =getAxis(align)
+            .tickSize(tickSize)
             .ticks(numTicks)
             .scale(scale)
-        
-        if (offset==0) {
-            xAxis.tickSize(tickSize)
-        }
 
-        const xLabel = parent.append("g")
+        xLabel = parent.append("g")
             .attr("class", "axis xAxis" )
             .call(xAxis)
-        
-        if (offset>0) {
-            xLabel.attr("transform","translate(0,"+(offset)+")");
-        }
-        
+           
     }
+    axis.align = (d)=>{
+        align=d;
+        return axis;
+    };
 
     axis.scale = (d)=>{
         scale = d;
         return axis;
-        }
-        axis.domain = (d)=>{
-            scale.domain(d);
-            return axis;
-        };
-        axis.range = (d)=>{
-            scale.range(d);
-            return axis;
-        };
-        
-        axis.tickSize = (d)=>{
-            if(d===undefined) return tickSize
-            tickSize=d;
-            return axis;
-        }
+    }
+    axis.domain = (d)=>{
+        scale.domain(d);
+        return axis;
+    };
+    axis.range = (d)=>{
+        scale.range(d);
+        return axis;
+    };
+    
+    axis.tickSize = (d)=>{
+        if(d===undefined) return tickSize
+        tickSize=d;
+        return axis;
+    }
 
-        axis.offset = (d)=>{
-            if(d===undefined) return offset
-            offset=d;
-            return axis;
-        }
+    axis.xLabel = (d)=>{
+        if(d===undefined) return xLabel
+        xLabel=d;
+        return axis;
+    }
 
-        axis.numTicks = (d)=>{
-            if(d===undefined) return numTicks
-            numTicks=d;
-            return axis;
-        }
+    axis.offset = (d)=>{
+        if(d===undefined) return offset
+        offset=d;
+        return axis;
+    }
+
+    axis.numTicks = (d)=>{
+        if(d===undefined) return numTicks
+        numTicks=d;
+        return axis;
+    }
+
+    function getAxis(alignment){
+        return{
+            "top": d3.axisTop(),
+            "bottom":d3.axisBottom()
+        }[alignment]
+    }
 
     
     return axis
--- bar/yAxisOrdinal.js
+++ bar/yAxisOrdinal.js
@@ -1,5 +1,5 @@
 function yAxisOrdinal() {
-    let tickAlign ="left"
+    let align ="left"
     let scale = d3.scaleBand()
         .domain(["Oranges", "Lemons", "Apples", "Pears"])
         .rangeRound([0, 220])
@@ -9,7 +9,7 @@ function yAxisOrdinal() {
 
     function axis(parent) {
 
-        const yAxis =getAxis(tickAlign)
+        const yAxis =getAxis(align)
             .tickSize(tickSize)
             .scale(scale)
 
@@ -28,48 +28,48 @@ function yAxisOrdinal() {
     axis.scale = (d)=>{
         scale = d;
         return axis;
-        }
-        axis.domain = (d)=>{
-            scale.domain(d);
-            return axis;
-        };
-        axis.rangeRound = (d)=>{
-            scale.rangeRound(d);
-            return axis;
-        };
-        axis.bandwidth = (  d)=>{
-            scale.bandwidth(d);
-            return axis;
-        };
+    }
+    axis.domain = (d)=>{
+        scale.domain(d);
+        return axis;
+    };
+    axis.rangeRound = (d)=>{
+        scale.rangeRound(d);
+        return axis;
+    };
+    axis.bandwidth = (  d)=>{
+        scale.bandwidth(d);
+        return axis;
+    };
 
-        axis.labelWidth = (d)=>{
-            if(d===undefined) return labelWidth
-            labelWidth=d;
-            return axis;
-        }
-        axis.yLabel = (d)=>{
-            if(d===undefined) return yLabel
-            labelWidth=d;
-            return axis;
-        }
-        
-        axis.tickSize = (d)=>{
-            if(d===undefined) return tickSize
-            tickSize=d;
-            return axis;
-        }
+    axis.labelWidth = (d)=>{
+        if(d===undefined) return labelWidth
+        labelWidth=d;
+        return axis;
+    }
+    axis.yLabel = (d)=>{
+        if(d===undefined) return yLabel
+        labelWidth=d;
+        return axis;
+    }
+    
+    axis.tickSize = (d)=>{
+        if(d===undefined) return tickSize
+        tickSize=d;
+        return axis;
+    }
 
-        axis.offset = (d)=>{
-            if(d===undefined) return offset
-            offset=d;
-            return axis;
-        }
+    axis.offset = (d)=>{
+        if(d===undefined) return offset
+        offset=d;
+        return axis;
+    }
 
-        axis.tickAlign = (d)=>{
-            if(!d) return tickAlign;
-            tickAlign = d;
-            return axis;
-        }
+    axis.align = (d)=>{
+        if(!d) return align;
+        align = d;
+        return axis;
+    }
 
     function getAxis(alignment){
         return{
