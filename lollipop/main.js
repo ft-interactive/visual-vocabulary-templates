23478d0500ae3f5071beb668622d63275aad3a3b
Alan Smith <alansmithobe@virginmedia.com>
2017-06-22 12:17:17 +0100

    fix xAxis domain issue

--- lollipop/main.js
+++ lollipop/main.js
@@ -4,6 +4,7 @@ function drawChart() {
 
     let yScale = d3.scaleLinear();
     let xScale = d3.scaleOrdinal();
+    let stalks = false;
 
     
     let seriesNames = [];
@@ -18,6 +19,8 @@ function drawChart() {
 
         //check colourScale is working
         parent.append("rect").attr("width",100).attr("height",100).attr("fill",colourScale(seriesNames[0]))
+        console.log(stalks)
+
 
     }
 
@@ -35,25 +38,28 @@ function drawChart() {
         yScale.domain(d);
         return chart;
     };
-
     chart.yRange = (d)=>{
         yScale.range(d);
         return chart;
     };
-
+    chart.stalks = (d)=>{
+        if(!d) return stalks;
+        stalks = d;
+        return chart;
+    };
     chart.seriesNames = (d)=>{
         seriesNames = d;
         return chart;
-    }
+    };
     chart.xScale = (d)=>{
         if(!d) return xScale;
         xScale = d;
         return chart;
-    }
+    };
      chart.xDomain = (d)=>{
          xScale.domain(d);
          return chart;
-     };
+    };
     chart.xRange = (d)=>{
         xScale.range(d);
         return chart;
@@ -62,12 +68,12 @@ function drawChart() {
         if(!d) return plotDim;
         plotDim = d;
         return chart;
-    }
+    };
     chart.rem = (d)=>{
         if(!d) return rem;
         rem = d;
         return chart;
-    }
+    };
     chart.colourPalette = (d) =>{
         if(d==='social' || d==='video'){
             colourScale.range(gChartcolour.lineSocial);
