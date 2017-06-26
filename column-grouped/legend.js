7cf4e77ddc117e393c8063689bc4f5cd38a359ba
Steve Bernard <steve.bernard@ft.com>
2017-06-26 18:46:59 +0100

    add check in legend.js for length of seriesnames to determine if a legend is necessary

--- column-grouped/legend.js
+++ column-grouped/legend.js
@@ -7,74 +7,77 @@ function drawLegend() {
     let alignment="hori"
     let geometry = "circ";
 
-	function legend(parent) {
-		let legendyOffset=0
 
-		parent.attr ("id",function(d,i){
-                return "l"+i
-            })
+    	function legend(parent) {
+             if (seriesNames.length > 1) {  
+        		let legendyOffset=0
 
-        parent.append("text")
-            .attr("id",(d,i)=> ("t"+i))
-			.attr("x",rem * 1.5)
-            .attr("y",rem/3)
-            .attr("class","chart-subtitle")
-            .text(function(d){
-                return d;
-            })
+        		parent.attr ("id",function(d,i){
+                        return "l"+i
+                    })
 
-        if(geometry==='rect') {    
-            parent.append('rect')
-                .attr('width', rem * 1.25)
-                .attr('height', rem/1.5)
-                .attr('class', 'rects')
-                .attr('x', 0)
-                .attr('y', -rem/3)
-                .style('fill', (d)=> colourScale(d))
-        }
+                parent.append("text")
+                    .attr("id",(d,i)=> ("t"+i))
+        			.attr("x",rem * 1.5)
+                    .attr("y",rem/3)
+                    .attr("class","chart-subtitle")
+                    .text(function(d){
+                        return d;
+                    })
 
-        else if(geometry === 'line') {
-            parent.append("line")
-                .attr("stroke",(d)=>colourScale(d))
-                .attr("x1",0)
-                .attr("x2",rem)
-                .attr("y1",0)
-                .attr("y2",0)
-                .attr("class","lines")
-        }
-
-        else if(geometry === 'circ') {
-            parent.append("circle")
-                .style("fill",(d)=>colourScale(d))
-                .attr("cx",rem - (rem/2.5))
-                .attr("cy",0)
-                .attr("r",rem/2.5)
-                .attr("class","circs")
-        }
-
-        parent.attr("transform",function(d,i){
-            console.log(alignment)
-            if (alignment=='hori') {
-                var gHeigt=d3.select("#l0").node().getBBox().height;
-                if (i>0) {
-                    var gWidth=d3.select("#l"+(i-1)).node().getBBox().width + (rem * 1.25); 
+                if(geometry==='rect') {    
+                    parent.append('rect')
+                        .attr('width', rem * 1.25)
+                        .attr('height', rem/1.5)
+                        .attr('class', 'rects')
+                        .attr('x', 0)
+                        .attr('y', -rem/3)
+                        .style('fill', (d)=> colourScale(d))
                 }
-                else {gWidth=0};
-                legendyOffset=legendyOffset+gWidth;
-                return "translate("+(legendyOffset)+","+(gHeigt/2)+")";  
+
+                else if(geometry === 'line') {
+                    parent.append("line")
+                        .attr("stroke",(d)=>colourScale(d))
+                        .attr("x1",0)
+                        .attr("x2",rem)
+                        .attr("y1",0)
+                        .attr("y2",0)
+                        .attr("class","lines")
+                }
+
+                else if(geometry === 'circ') {
+                    parent.append("circle")
+                        .style("fill",(d)=>colourScale(d))
+                        .attr("cx",rem - (rem/2.5))
+                        .attr("cy",0)
+                        .attr("r",rem/2.5)
+                        .attr("class","circs")
+                }
+
+                parent.attr("transform",function(d,i){
+                    console.log(alignment)
+                    if (alignment=='hori') {
+                        var gHeigt=d3.select("#l0").node().getBBox().height;
+                        if (i>0) {
+                            var gWidth=d3.select("#l"+(i-1)).node().getBBox().width + (rem * 1.25); 
+                        }
+                        else {gWidth=0};
+                        legendyOffset=legendyOffset+gWidth;
+                        return "translate("+(legendyOffset)+","+(gHeigt/2)+")";  
+                    }
+                    else {
+                        return "translate(0,"+((i * rem * 1.2))+")"};
+                })
             }
-            else {
-                return "translate(0,"+((i * rem * 1.2))+")"};
-        })
+    	}
 
-	}
-
-   d3.selectAll("#legend")
-        .on("mouseover",pointer)
-        .call(d3.drag()
-                .on("start", dragstarted)
-                .on("drag", dragged)
-                .on("end", dragended));
+       d3.selectAll("#legend")
+            .on("mouseover",pointer)
+            .call(d3.drag()
+                    .on("start", dragstarted)
+                    .on("drag", dragged)
+                    .on("end", dragended));
+   
 
 	legend.seriesNames = (d)=>{
         seriesNames = d;
