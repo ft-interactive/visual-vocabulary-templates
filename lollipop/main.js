5aae70e6155e58d84150dabd7f9a96573106069f
Alan Smith <alansmithobe@virginmedia.com>
2017-06-26 08:07:20 +0100

    create stalk components

--- lollipop/main.js
+++ lollipop/main.js
@@ -1,9 +1,10 @@
 function drawChart() {
 
     let yScale = d3.scaleLinear();
-    let xScale = d3.scaleOrdinal();
+    let xScale = d3.scaleBand()
     let stalks = false;
-
+    let stalkWidth = .1;
+    let dotRadius = .5;
     
     let seriesNames = [];
     let yAxisAlign = "right"
@@ -11,14 +12,34 @@ function drawChart() {
 
     const colourScale = d3.scaleOrdinal()
         .domain(seriesNames);
-
   
     function chart(parent){
 
+        parent.append("circle")
+            .attr("cx",function(d){
+                return xScale(d.name);
+            })
+            .attr("cy", function(d){
+                return yScale(d[seriesNames[0]])
+            })
+            .attr("r",dotRadius*xScale.bandwidth())
+            .attr("fill",colourScale(seriesNames[0]))
 
-
-
-    //colourScale(seriesNames[0])
+        if (stalks) {
+            parent.append("line")
+                .attr("x1",function(d){
+                    return xScale(d.name)
+                })
+                .attr("x2",function(d){
+                    return xScale(d.name)
+                })
+               .attr("y1",yScale(0))
+               .attr("y2",function(d){
+                    return yScale(d[seriesNames[0]])
+               })
+               .attr("stroke-width",stalkWidth*xScale.bandwidth())
+               .attr("stroke",colourScale(seriesNames[0]))
+        }
 
 
 
@@ -45,6 +66,16 @@ function drawChart() {
     chart.stalks = (d)=>{
         if(!d) return stalks;
         stalks = d;
+        return chart;
+    };
+    chart.stalkWidth = (d)=>{
+        if(!d) return stalkWidth;
+        stalkWidth = d;
+        return chart;
+    };
+    chart.dotRadius = (d)=>{
+        if(!d) return dotRadius;
+        dotRadius = d;
         return chart;
     };
     chart.seriesNames = (d)=>{
