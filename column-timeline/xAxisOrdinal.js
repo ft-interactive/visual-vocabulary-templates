fba3cdead4020ae0065a73f94d4b952c753f3716
Bob Haslett <bob.haslett@ft.com>
2017-06-28 17:21:38 +0100

    Merge branch 'master' into Add-Bar-chart
    * master: (82 commits)
      tweak local asset chartcolour lineprint palette
      update local resources for wrapper starter
      update legendtype comment on wrapper starter
      update legendType comment
      change wrapper starter to include legend package and update local
    assets
      Adds EditorConfig, ESLint, TernJS (#8)
      remove unnecessary parameter
      add legend package to line slope and column templates
      add legend pacjage
      add in legend package to replace local js
      add extra date to end of csv
      add instruction about adding extra date
      add IDs to data
      sort bandwidth settings
      add date axis
      make a copy of column grouped template
      add check in legend.js for length of seriesnames to determine if a
    legend is necessary
      add some space between xaxis and labels
      renamed folder to column-grouped
      remove unnecessary settings
      ...
    
    # Conflicts:
    #	line/yaxislinear.js

---
+++ column-timeline/xAxisOrdinal.js
@@ -0,0 +1,93 @@
+function xAxisOrdinal() {
+    let align ="bottom"
+    let scale = d3.scaleBand()
+        .domain(["Oranges", "Lemons", "Apples", "Pears"])
+        .rangeRound([0, 220])
+        .paddingInner(0.1)
+        .paddingOuter(0.05)
+    let labelWidth = 0;
+    let tickSize = 0;
+    let offset = 0;
+
+    function axis(parent) {
+
+        const xAxis =getAxis(align)
+            .tickSize(tickSize)
+            .scale(scale)
+
+        if (scale.domain.length > 1) {
+            scale.paddingInner(0.1)
+        } else {
+            scale.paddingInner(0.2)
+        }
+
+        xLabel = parent.append("g")
+            .attr("class","axis xAxis")
+            .attr('transform','translate(0,' + offset +')' )
+            .call(xAxis)
+    }
+
+    axis.scale = (d)=>{
+        if(!d) return scale;
+        scale = d;
+        return axis;
+    }
+    axis.domain = (d)=>{
+        scale.domain(d);
+        return axis;
+    };
+    axis.rangeRound = (d)=>{
+        scale.rangeRound(d);
+        return axis;
+    };
+    axis.bandwidth = (  d)=>{
+        if (!d) return scale.bandwidth();
+        scale.bandwidth(d);
+        return axis;
+    };
+
+    axis.paddingInner = (  d)=>{
+        if (!d) return scale.paddingInner();
+        scale.paddingInner(d);
+        return axis;
+    };
+    
+    axis.paddingOuter = (  d)=>{
+            if (!d) return scale.paddinguter();
+            scale.paddinguter(d);
+            return axis;
+        };
+
+    axis.xLabel = (d)=>{
+        if(d===undefined) return yLabel
+        labelWidth=d;
+        return axis;
+    }
+    
+    axis.tickSize = (d)=>{
+        if(d===undefined) return tickSize
+        tickSize=d;
+        return axis;
+    }
+
+    axis.offset = (d)=>{
+        if(d===undefined) return offset
+        offset=d;
+        return axis;
+    }
+
+    axis.align = (d)=>{
+        if(!d) return align;
+        align = d;
+        return axis;
+    }
+
+    function getAxis(alignment){
+        return{
+            "top": d3.axisTop(),
+            "bottom":d3.axisBottom()
+        }[alignment]
+    }
+    
+    return axis
+};
---
+++ wrapper-starter/main.js
@@ -0,0 +1,89 @@
+function drawChart() {
+    let yScale;
+    let xScale;
+    let seriesNames = [];
+    let yAxisAlign = "right"
+    let rem =16;
+    let markers = false;
+    // let interpolation =d3.curveLinear
+    const colourScale = d3.scaleOrdinal()
+        .domain(seriesNames);
+  
+    function chart(parent){
+
+        
+    }
+
+    chart.yScale = (d)=>{
+        if(!d) return yScale;
+        yScale = d;
+        return chart;
+    }
+    chart.yAxisAlign = (d)=>{
+        if(!d) return yAxisAlign;
+        yAxisAlign = d;
+        return chart;
+    }
+    chart.yDomain = (d)=>{
+        yScale.domain(d);
+        return chart;
+    };
+
+    chart.yRange = (d)=>{
+        yScale.range(d);
+        return chart;
+    };
+
+    chart.seriesNames = (d)=>{
+        seriesNames = d;
+        return chart;
+    }
+    chart.xScale = (d)=>{
+        if(!d) return xScale;
+        xScale = d;
+        return chart;
+    }
+    chart.xDomain = (d)=>{
+        xScale.domain(d);
+        return chart;
+    };
+    chart.xRange = (d)=>{
+        xScale.range(d);
+        return chart;
+    };
+    chart.plotDim = (d)=>{
+        if(!d) return plotDim;
+        plotDim = d;
+        return chart;
+    }
+    chart.rem = (d)=>{
+        if(!d) return rem;
+        rem = d;
+        return chart;
+    }
+    chart.includeMarker = (d)=>{
+        includeMarker = d;
+        return chart;
+    }
+    chart.markers = (d)=>{
+        markers = d;
+        return chart;
+    }
+    chart.interpolation = (d)=>{
+        if(!d) return interpolation;
+        interpolation = d;
+        return chart;
+    }
+    chart.colourPalette = (d) =>{
+        if(d==='social' || d==='video'){
+            colourScale.range(gChartcolour.lineSocial);
+        } else if (d==='webS' || d==='webM' || d==='webL') {
+            colourScale.range(gChartcolour.lineWeb);
+        } else if (d==='print') {
+            colourScale.range(gChartcolour.linePrint);
+        }
+        return chart;
+    }
+
+    return chart
+}
