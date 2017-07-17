28198b6fac7fbdd3232c6e305af1f15f4892727f
Ændrew Rininsland <andrew.rininsland@ft.com>
2017-07-17 12:52:34 +0100

    Updating lollipop-v

--- line/parseData.js
+++ line/parseData.js
@@ -5,7 +5,7 @@ export function fromCSV(url, dateStructure) {
         d3.csv(url, (error, data) => {
             if (error) reject(error);
             else {
-        // make sure all the dates in the date column are a date object
+                // make sure all the dates in the date column are a date object
                 const parseDate = d3.timeParse(dateStructure);
                 data.forEach((d) => {
                     d.date = parseDate(d.date);
