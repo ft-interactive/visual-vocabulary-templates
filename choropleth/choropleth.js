375c419f0718af44ea596481c797571236c6a94d
Ændrew Rininsland <andrew.rininsland@ft.com>
2017-09-06 18:09:51 +0100

    Recent

--- choropleth/choropleth.js
+++ choropleth/choropleth.js
@@ -1,71 +1,21 @@
 import * as d3 from 'd3';
-import gChartcolour from 'g-chartcolour';
-
-let rem = 10;
+import * as gChartcolour from 'g-chartcolour';
 
 export function draw() {
-    let yScale = d3.scaleLinear();
-    let xScale = d3.scaleTime();
+    let yScale;
+    let xScale;
     let seriesNames = [];
-    let highlightNames = [];
     let yAxisAlign = 'right';
-    let markers = false;
-  const includeAnnotations = d => (d.annotate !== '' && d.annotate !== undefined); // eslint-disable-line
-  let annotate = false; // eslint-disable-line
-    let interpolation = d3.curveLinear;
+    let rem = 16;
+    let markers = false; // eslint-disable-line
+    let includeMarker = false; // eslint-disable-line
+    let interpolation = false;
+
+    // let interpolation =d3.curveLinear
     const colourScale = d3.scaleOrdinal()
-    // .range(gChartcolour.lineWeb)
-    .domain(seriesNames);
+        .domain(seriesNames);
 
-    function chart(parent) {
-
-        const lineData = d3.line()
-        .defined(function(d) { return d; })
-        .curve(interpolation)
-        .x(d => xScale(d.date))
-        .y(d => yScale(d.value));
-
-        parent.append('path')
-      .attr('stroke', (d) => {
-          if (highlightNames.length > 0) {
-              if (highlightNames.indexOf(d.name) !== -1) {
-                  return colourScale(d.name);
-              }
-              d.name = '';
-              return colourScale(d.name);
-          }
-          return colourScale(d.name);
-      })
-      .attr('opacity', (d) => {
-          if (highlightNames.length > 0) {
-              if (highlightNames.indexOf(d.name) !== -1) {
-                  return 1;
-              }
-              return 0.5;
-          }
-          return 1;
-      })
-      .attr('d', d => lineData(d.lineData));
-
-        if (markers) {
-            parent.selectAll('.markers')
-        .data((d) => {
-            if (markers) {
-                return d.lineData;
-            }
-
-            return undefined;
-        })
-        .enter()
-        .append('circle')
-        .classed('markers', true)
-        .attr('id', d => `date: ${d.date} value: ${d.value}`)
-        .attr('cx', d => xScale(d.date))
-        .attr('cy', d => yScale(d.value))
-        .attr('r', rem * 0.25)
-        .attr('fill', d => colourScale(d.name));
-        }
-    }
+    function chart(parent) { /** Main rendering function here **/ } // eslint-disable-line
 
     chart.yScale = (d) => {
         if (!d) return yScale;
@@ -78,21 +28,15 @@ export function draw() {
         return chart;
     };
     chart.yDomain = (d) => {
-        if (!d) return yDomain;
         yScale.domain(d);
         return chart;
     };
 
     chart.yRange = (d) => {
-        if (!d) return yRange;
         yScale.range(d);
         return chart;
     };
 
-    chart.highlightNames = (d) => {
-        highlightNames = d;
-        return chart;
-    };
     chart.seriesNames = (d) => {
         seriesNames = d;
         return chart;
@@ -103,12 +47,10 @@ export function draw() {
         return chart;
     };
     chart.xDomain = (d) => {
-        if (!d) return xDomain;
         xScale.domain(d);
         return chart;
     };
     chart.xRange = (d) => {
-        if (!d) return xRange;
         xScale.range(d);
         return chart;
     };
@@ -122,8 +64,8 @@ export function draw() {
         rem = d;
         return chart;
     };
-    chart.annotate = (d) => {
-        annotate = d;
+    chart.includeMarker = (d) => {
+        includeMarker = d;
         return chart;
     };
     chart.markers = (d) => {
@@ -136,19 +78,9 @@ export function draw() {
         return chart;
     };
     chart.colourPalette = (d) => {
-        if (highlightNames.length > 0) {
-            if (d === 'social' || d === 'video') {
-                colourScale.range(gChartcolour.mutedFirstLineSocial);
-            } else if (d === 'webS' || d === 'webM' || d === 'webMDefault' || d === 'webL') {
-                colourScale.range(gChartcolour.mutedFirstLineWeb);
-            } else if (d === 'print') {
-                colourScale.range(gChartcolour.mutedFirstLinePrint);
-            }
-            return chart;
-        }
         if (d === 'social' || d === 'video') {
             colourScale.range(gChartcolour.lineSocial);
-        } else if (d === 'webS' || d === 'webM' || d === 'webMDefault' || d === 'webL') {
+        } else if (d === 'webS' || d === 'webM' || d === 'webL') {
             colourScale.range(gChartcolour.lineWeb);
         } else if (d === 'print') {
             colourScale.range(gChartcolour.linePrint);
@@ -157,97 +89,4 @@ export function draw() {
     };
 
     return chart;
-}
-
-export function drawHighlights() {
-    let yScale = d3.scaleLinear();
-    let xScale = d3.scaleTime();
-    let invertScale = false
-
-    function highlights(parent) {
-        let highlights = parent.append('rect')
-        .attr('class', 'highlights')
-        .attr('x', d => xScale(d.begin))
-        .attr('width', d => xScale(d.end) - xScale(d.begin))
-        .attr('y', function(d) {
-            if(invertScale) {
-                return yScale.range()[0]
-            }
-              return yScale.range()[1]
-            })
-        .attr('height', function (d) {
-            if(invertScale) {
-                return yScale.range()[1]
-            }
-            return yScale.range()[0]
-        })
-        .attr('fill', '#fff1e0');
-    }
-
-    highlights.yScale = (d) => {
-        yScale = d;
-        return highlights;
-    };
-    highlights.xScale = (d) => {
-        xScale = d;
-        return highlights;
-    };
-    highlights.yRange = (d) => {
-        yScale.range(d);
-        return highlights;
-    };
-    highlights.xRange = (d) => {
-        xScale.range(d);
-        return highlights;
-    };
-    highlights.invertScale = (d) => {
-        invertScale = d;
-        return highlights;
-    };
-
-    return highlights;
-}
-
-export function drawAnnotations() {
-    let yScale = d3.scaleLinear();
-    let xScale = d3.scaleTime();
-
-    function annotations(parent) {
-        parent.append('line')
-      .attr('class', 'annotation')
-      .attr('x1', d => xScale(d.date))
-      .attr('x2', d => xScale(d.date))
-      .attr('y1', yScale.range()[0])
-      .attr('y2', yScale.range()[1] - 5);
-
-        parent.append('text')
-      .attr('class', 'annotation')
-      .attr('text-anchor', 'middle')
-      .attr('x', d => xScale(d.date))
-      .attr('y', yScale.range()[1] - (rem / 2))
-      .text(d => d.annotate);
-    }
-
-    annotations.yScale = (d) => {
-        yScale = d;
-        return annotations;
-    };
-    annotations.xScale = (d) => {
-        xScale = d;
-        return annotations;
-    };
-    annotations.yRange = (d) => {
-        yScale.range(d);
-        return annotations;
-    };
-    annotations.xRange = (d) => {
-        xScale.range(d);
-        return annotations;
-    };
-    annotations.rem = (d) => {
-        if (!d) return rem;
-        rem = d;
-        return annotations;
-    };
-    return annotations;
 }
--- choropleth/parseData.js
+++ choropleth/parseData.js
@@ -9,50 +9,44 @@ import * as d3 from 'd3';
  * @param  {String} url Path to CSV file
  * @return {Object}     Object containing series names, value extent and raw data object
  */
-export function fromCSV(url, dateStructure) {
+export function fromCSV(url, dateFormat) {
     return new Promise((resolve, reject) => {
         d3.csv(url, (error, data) => {
             if (error) reject(error);
             else {
                 // make sure all the dates in the date column are a date object
-                const parseDate = d3.timeParse(dateStructure);
+                const parseDate = d3.timeParse(dateFormat);
+
                 data.forEach((d) => {
                     d.date = parseDate(d.date);
                 });
 
-                resolve(data);
+                // automatically calculate the seriesnames excluding the "marker" and "annotate column"
+                const seriesNames = getSeriesNames(data.columns);
+
+                // Use the seriesNames array to calculate the minimum and max values in the dataset
+                const valueExtent = extentMulti(data, seriesNames);
+
+                resolve({
+                    seriesNames,
+                    valueExtent,
+                    data,
+                });
             }
         });
     });
 }
 
-
-/**
- * Returns the columns headers from the top of the dataset, excluding specified
- * @param  {[type]} columns [description]
- * @return {[type]}         [description]
- */
-export function getSeriesNames(columns) {
-    const exclude = ['date', 'annotate', 'highlight'];
+// a function that returns the columns headers from the top of the dataset, excluding specified
+function getSeriesNames(columns) {
+    const exclude = ['date']; // adjust column headings to match your dataset
     return columns.filter(d => (exclude.indexOf(d) === -1));
 }
 
-/**
- * Calculates the extent of multiple columns
- * @param  {[type]} d       [description]
- * @param  {[type]} columns [description]
- * @param  {[type]} yMin    [description]
- * @return {[type]}         [description]
- */
-export function extentMulti(d, columns, yMin) {
-    const ext = d.reduce((acc, row) => {
-        let values = columns.map(key => row[key])
-        .map((item) => {
-            if (!item || item === '*') {
-                return yMin;
-            }
-            return Number(item);
-        });
+// a function to work out the extent of values in an array accross multiple properties...
+function extentMulti(data, columns) {
+    const ext = data.reduce((acc, row) => {
+        const values = columns.map(key => +row[key]);
         const rowExtent = d3.extent(values);
         if (!acc.max) {
             acc.max = rowExtent[1];
@@ -64,42 +58,4 @@ export function extentMulti(d, columns, yMin) {
         return acc;
     }, {});
     return [ext.min, ext.max];
-}
-
-/**
- * Sorts the column information in the dataset into groups according to the column
- * head, so that the line path can be passed as one object to the drawing function
- */
-export function getlines(d, group) {
-    let lineData=[]
-    d.forEach(function(el,i){
-        //console.log(el,i)
-        let column=new Object();
-        column.name = group
-        column.date = el.date
-        column.value = +el[group]
-        column.highlight = el.highlight
-        column.annotate = el.annotate
-        if(el[group]) {
-            lineData.push(column)  
-        }
-        if(el[group] == false) {
-            lineData.push(null)  
-        }
-
-    });
-    return lineData
-    // return d.map((el) => {
-    //     if (el[group]) {
-    //         return {
-    //             name: group,
-    //             date: el.date,
-    //             value: +el[group],
-    //             highlight: el.highlight,
-    //             annotate: el.annotate,
-    //         };
-    //     }
-
-    //     return null;
-    // }).filter(i => i);
 }
