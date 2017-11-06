b278991691233b5ba91020bde058a26c3bec62b6
Ændrew Rininsland <aendrew@aendrew.com>
2017-11-06 18:54:50 +0000

    Adds webM-default to wrapper-starter (#65)

--- wrapper-starter/index.js
+++ wrapper-starter/index.js
@@ -28,43 +28,48 @@ const sharedConfig = {
 
 // Individual frame configuratiuon, used to set margins (defaults shown below) etc
 const frame = {
+    webMDefault: gChartframe.webFrameMDefault(sharedConfig)
+        .margin({ top: 10, left: 10, bottom: 80, right: 5 })
+        // .title("Put headline here")
+        .height(800),
+
     webS: gChartframe.webFrameS(sharedConfig)
-   .margin({ top: 100, left: 15, bottom: 82, right: 5 })
-   // .title("Put headline here") //use this if you need to override the defaults
-   // .subtitle("Put headline |here") //use this if you need to override the defaults
-   .height(400),
+       .margin({ top: 100, left: 15, bottom: 82, right: 5 })
+       // .title("Put headline here") //use this if you need to override the defaults
+       // .subtitle("Put headline |here") //use this if you need to override the defaults
+       .height(400),
 
     webM: gChartframe.webFrameM(sharedConfig)
-   .margin({ top: 100, left: 20, bottom: 86, right: 5 })
-   // .title("Put headline here")
-   .height(500),
+        .margin({ top: 100, left: 20, bottom: 86, right: 5 })
+        // .title("Put headline here")
+        .height(500),
 
     webL: gChartframe.webFrameL(sharedConfig)
-   .margin({ top: 100, left: 20, bottom: 104, right: 5 })
-   // .title("Put headline here")
-   .height(700)
-   .fullYear(true),
+        .margin({ top: 100, left: 20, bottom: 104, right: 5 })
+        // .title("Put headline here")
+        .height(700)
+        .fullYear(true),
 
     print: gChartframe.printFrame(sharedConfig)
-    .margin({ top: 40, left: 7, bottom: 35, right: 7 })
-    // .title("Put headline here")
-    // .width(53.71)// 1 col
-    .width(112.25)// 2 col
-    // .width(170.8)// 3 col
-    // .width(229.34)// 4 col
-    // .width(287.88)// 5 col
-    // .width(346.43)// 6 col
-    // .width(74)// markets std print
-    .height(58.21), // markets std print
+        .margin({ top: 40, left: 7, bottom: 35, right: 7 })
+        // .title("Put headline here")
+        // .width(53.71)// 1 col
+        .width(112.25)// 2 col
+        // .width(170.8)// 3 col
+        // .width(229.34)// 4 col
+        // .width(287.88)// 5 col
+        // .width(346.43)// 6 col
+        // .width(74)// markets std print
+        .height(58.21), // markets std print
 
     social: gChartframe.socialFrame(sharedConfig)
-   .margin({ top: 140, left: 50, bottom: 138, right: 40 })
-   // .title("Put headline here")
-   .height(750), // 700 is ideal height for Instagram
+        .margin({ top: 140, left: 50, bottom: 138, right: 40 })
+        // .title("Put headline here")
+        .height(750), // 700 is ideal height for Instagram
 
     video: gChartframe.videoFrame(sharedConfig)
-   .margin({ left: 207, right: 207, bottom: 210, top: 233 }),
-   // .title("Put headline here")
+        .margin({ left: 207, right: 207, bottom: 210, top: 233 }),
+        // .title("Put headline here")
 };
 
 // add the frames to the page...
