d3865c9228baadc7b3c8514ba1c54db2846c753b
Steve Bernard <steve.bernard@ft.com>
2017-11-28 14:14:58 +0000

    linting, strip out unused stuff

--- treemap/index.js
+++ treemap/index.js
@@ -18,8 +18,8 @@ const sharedConfig = {
     source: 'Source not yet added',
 };
 
-const yMin = 0;// sets the minimum value on the yAxis
-const yMax = 0;// sets the maximum value on the xAxis
+// const yMin = 0;// sets the minimum value on the yAxis
+// const yMax = 0;// sets the maximum value on the xAxis
 const legendAlign = 'hori';// hori or vert, alignment of the legend
 const legendType = 'rect';// rect, line or circ, geometry of legend marker
 
@@ -27,117 +27,127 @@ const legendType = 'rect';// rect, line or circ, geometry of legend marker
 // Individual frame configuration, used to set margins (defaults shown below) etc
 const frame = {
     webS: gChartframe.webFrameS(sharedConfig)
- .margin({ top: 100, left: 0, bottom: 82, right: 5 })
- // .title('Put headline here') // use this if you need to override the defaults
- // .subtitle("Put headline |here") //use this if you need to override the defaults
- .height(400),
+        .margin({
+            top: 100, left: 0, bottom: 82, right: 5,
+        })
+    // .title('Put headline here') // use this if you need to override the defaults
+    // .subtitle("Put headline |here") //use this if you need to override the defaults
+        .height(400),
 
     webM: gChartframe.webFrameM(sharedConfig)
- .margin({ top: 100, left: 0, bottom: 86, right: 5 })
- // .title("Put headline here")
- .height(500),
+        .margin({
+            top: 100, left: 0, bottom: 86, right: 5,
+        })
+    // .title("Put headline here")
+        .height(500),
 
     webL: gChartframe.webFrameL(sharedConfig)
- .margin({ top: 100, left: 0, bottom: 104, right: 5 })
- // .title("Put headline here")
- .height(700)
- .fullYear(true),
+        .margin({
+            top: 100, left: 0, bottom: 104, right: 5,
+        })
+    // .title("Put headline here")
+        .height(700)
+        .fullYear(true),
 
     webMDefault: gChartframe.webFrameMDefault(sharedConfig)
- .margin({ top: 100, left: 0, bottom: 86, right: 5 })
- // .title("Put headline here")
- .height(500),
+        .margin({
+            top: 100, left: 0, bottom: 86, right: 5,
+        })
+    // .title("Put headline here")
+        .height(500),
 
     print: gChartframe.printFrame(sharedConfig)
- .margin({ top: 40, left: 7, bottom: 35, right: 7 })
-  // .title("Put headline here")
-  // .width(53.71)// 1 col
-  .width(229.34)// 2 col
-  // .width(170.8)// 3 col
-  // .width(229.34)// 4 col
-  // .width(287.88)// 5 col
-  // .width(346.43)// 6 col
-  // .width(74)// markets std print
-  .height(200), // markets std print
+        .margin({
+            top: 40, left: 7, bottom: 35, right: 7,
+        })
+    // .title("Put headline here")
+    // .width(53.71)// 1 col
+        .width(229.34)// 2 col
+    // .width(170.8)// 3 col
+    // .width(229.34)// 4 col
+    // .width(287.88)// 5 col
+    // .width(346.43)// 6 col
+    // .width(74)// markets std print
+        .height(200), // markets std print
 
     social: gChartframe.socialFrame(sharedConfig)
- .margin({ top: 140, left: 50, bottom: 138, right: 40 })
- // .title("Put headline here")
- .width(612)
- .height(612), // 700 is ideal height for Instagram
+        .margin({
+            top: 140, left: 50, bottom: 138, right: 40,
+        })
+    // .title("Put headline here")
+        .width(612)
+        .height(612), // 700 is ideal height for Instagram
 
     video: gChartframe.videoFrame(sharedConfig)
- .margin({ left: 207, right: 207, bottom: 210, top: 233 }),
- // .title("Put headline here")
+        .margin({
+            left: 207, right: 207, bottom: 210, top: 233,
+        }),
+    // .title("Put headline here")
 };
 
 
 // add the frames to the page...
 d3.selectAll('.framed')
-  .each(function addFrames() {
-      const figure = d3.select(this);
-      figure.select('svg')
-          .call(frame[figure.node().dataset.frame]);
-  });
+    .each(function addFrames() {
+        const figure = d3.select(this);
+        figure.select('svg')
+            .call(frame[figure.node().dataset.frame]);
+    });
 parseData.load(dataFile)
-.then(({ seriesNames, data, plotData}) => {
-    Object.keys(frame).forEach((frameName) => {
-        const currentFrame = frame[frameName];
+    .then(({ seriesNames, data, plotData }) => {
+        Object.keys(frame).forEach((frameName) => {
+            const currentFrame = frame[frameName];
 
-        // define other functions to be called
-        const myLegend = gLegend.legend();// sets up the legend
-        // const plotDim=currentFrame.dimension()//useful variable to carry the current frame dimensions
-        const tickSize = currentFrame.dimension().width;// Used when drawing the yAxis ticks
-        const myTreemap = treemap.draw()
+            // define other functions to be called
+            const myLegend = gLegend.legend();// sets up the legend
+            // const plotDim=currentFrame.dimension()//useful variable to carry the current frame dimensions
+            // const tickSize = currentFrame.dimension().width;// Used when drawing the yAxis ticks
+            const myTreemap = treemap.draw();
 
-        // create a 'g' element at the back of the chart to add time period
-        // highlights after axis have been created
-        const axisHighlight = currentFrame.plot().append('g');
+            // create a 'g' element at the back of the chart to add time period
+            // highlights after axis have been created
+            const axisHighlight = currentFrame.plot().append('g');
 
-        axisHighlight.append("rect")
-          .attr("width", currentFrame.dimension().width)
-          .attr("height",currentFrame.dimension().height)
-          .attr("fill","#ededee");
+            axisHighlight.append('rect')
+                .attr('width', currentFrame.dimension().width)
+                .attr('height', currentFrame.dimension().height)
+                .attr('fill', '#ededee');
 
-        myTreemap
-          .plotDim(currentFrame.dimension())
-          .seriesNames(seriesNames)
-          .rem(currentFrame.rem())
-          .colourPalette((frameName));
+            myTreemap
+                .plotDim(currentFrame.dimension())
+                .seriesNames(seriesNames)
+                .rem(currentFrame.rem())
+                .colourPalette((frameName));
 
-        currentFrame.plot()
-          .selectAll('.treemap')
-          .data(plotData)
-          .enter()
-          .append('g')
-          .attr('class', 'treemap')
-          .call(myTreemap)
+            currentFrame.plot()
+                .selectAll('.treemap')
+                .data(plotData)
+                .enter()
+                .append('g')
+                .attr('class', 'treemap')
+                .call(myTreemap);
 
-        myLegend
-            .seriesNames(seriesNames)
-            .geometry(legendType)
-            .frameName(frameName)
-            .rem(currentFrame.rem())
-            .alignment(legendAlign)
-            .colourPalette((frameName));
+            myLegend
+                .seriesNames(seriesNames)
+                .geometry(legendType)
+                .frameName(frameName)
+                .rem(currentFrame.rem())
+                .alignment(legendAlign)
+                .colourPalette((frameName));
 
-        currentFrame.plot()
-            .append('g')
-            .attr('id', 'legend')
+            currentFrame.plot()
+                .append('g')
+                .attr('id', 'legend')
                 .selectAll('.legend')
                 .data(seriesNames)
                 .enter()
                 .append('g')
                 .classed('legend', true)
-            .call(myLegend);
+                .call(myLegend);
 
-        const legendSelection = currentFrame.plot().select('#legend');
+            const legendSelection = currentFrame.plot().select('#legend');
         const legheight = (legendSelection.node().getBBox().height); // eslint-disable-line
-        legendSelection.attr('transform', `translate(0,${-currentFrame.rem()})`);
-
-        
-        
-
-    });
+            legendSelection.attr('transform', `translate(0,${-currentFrame.rem()})`);
+        });
     // addSVGSavers('figure.saveable');
-});
+    });
--- treemap/treemap.js
+++ treemap/treemap.js
@@ -7,55 +7,54 @@ export function draw() {
     let seriesNames = [];
     const colourScale = d3.scaleOrdinal()
     // .range(gChartcolour.lineWeb)
-    .domain(seriesNames);
+        .domain(seriesNames);
 
     function chart(parent) {
         const width = plotDim.width;
         const height = plotDim.height;
 
-        let treemap = d3.treemap().size([width, height]).paddingInner(1);
+        const treemap = d3.treemap().size([width, height]).paddingInner(1);
 
-        let newData = parent.data()[0]
+        const newData = parent.data()[0];
 
-        var root = d3.hierarchy(newData, (d) => d.children)
-            .sum(function (d) {
-                return d.value;
-            })
+        const root = d3.hierarchy(newData, d => d.children)
+            .sum(d => d.value);
             // .sort(function (a, b) {
             //     console.log (a,b)
             //     return b.value - a.value || b.value - a.value;
             // });
- 
+
         treemap(root);
 
-        var map = parent.append('g')
+        const map = parent.append('g')
             .attr('id', 'map')
-            .attr('width', width )
-            .attr('height', height)
+            .attr('width', width)
+            .attr('height', height);
 
-        var cell = map.selectAll('g')
+        const cell = map.selectAll('g')
             .data(root.leaves())
             .enter()
             .append('g');
 
         cell.append('rect')
-        .attr('class', 'bars')
-        .attr('x', (d) => {return d.x0;})
-        .attr('y', d =>  d.y0)
-        .attr('width', (d) => {return d.x1 - d.x0;})
-        .attr('height', (d) => {return d.y1 - d.y0;})
-        .attr('fill', (d) => {return colourScale(d.data.category)});
+            .attr('class', 'bars')
+            .attr('x', d => d.x0)
+            .attr('y', d => d.y0)
+            .attr('width', d => d.x1 - d.x0)
+            .attr('height', d => d.y1 - d.y0)
+            .attr('fill', d => colourScale(d.data.category));
 
         cell.append('text')
-        .attr('dx',(d) => {return d.x0 +(rem/2);})
-        .attr('dy',(d) => {return d.y0 +rem;})
-        .attr('font-size', rem)
-        .attr('class', 'highlight-label')
-        .text((d) => {
-            if (d.data.highlight) {
-                return d.data.name + ' ' + d.value}
-            })
-        }
+            .attr('dx', d => d.x0 + (rem / 2))
+            .attr('dy', d => d.y0 + rem)
+            .attr('font-size', rem)
+            .attr('class', 'highlight-label')
+            .text((d) => {
+                if (d.data.highlight) {
+                    return `${d.data.name} ${d.value}`;
+                }
+            });
+    }
 
     chart.seriesNames = (d) => {
         seriesNames = d;
