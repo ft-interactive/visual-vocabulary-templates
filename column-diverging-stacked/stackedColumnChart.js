258894b529ff534abd3200f45c01ac16b1fc4e84
Steve Bernard <steve.bernard@ft.com>
2017-12-05 13:04:05 +0000

    use column stacked as a starting point

---
+++ column-diverging-stacked/stackedColumnChart.js
@@ -0,0 +1,122 @@
+import * as d3 from 'd3';
+import gChartcolour from 'g-chartcolour';
+
+export function draw() {
+    let yScale = d3.scaleLinear();
+    let xScale = d3.scaleBand();
+    let seriesNames = [];
+    let yAxisAlign = 'right';
+    let rem = 16;
+    let markers = false; // eslint-disable-line
+    let includeMarker = undefined; // eslint-disable-line
+    let interpolation = d3.curveLinear;
+    const colourScale = d3.scaleOrdinal()
+        .domain(seriesNames);
+
+    function chart(parent) {
+        parent.attr('transform', d => `translate(${xScale(d.name)},0)`)
+            .attr('width', xScale.bandwidth());
+
+        parent.selectAll('rect')
+            .data(d => d.bands)
+            .enter()
+            .append('rect')
+            .attr('width', xScale.bandwidth())
+            .attr('x', d => xScale(d.name))
+            .attr('y', d => yScale(Math.max(d.y, d.y1)))
+            .attr('height', d => Math.abs(yScale(0) - yScale(d.value)))
+            .attr('fill', d => colourScale(d.name));
+    }
+
+    chart.yScale = (d) => {
+        if (!d) return yScale;
+        yScale = d;
+        return chart;
+    };
+
+    chart.yDomain = (d) => {
+        if (typeof d === 'undefined') return yScale.domain();
+        yScale.domain(d);
+        return chart;
+    };
+
+    chart.yRange = (d) => {
+        if (typeof d === 'undefined') return yScale.range();
+        yScale.range(d);
+        return chart;
+    };
+
+    chart.yAxisAlign = (d) => {
+        if (!d) return yAxisAlign;
+        yAxisAlign = d;
+        return chart;
+    };
+
+    chart.seriesNames = (d) => {
+        if (typeof d === 'undefined') return seriesNames;
+        seriesNames = d;
+        return chart;
+    };
+
+    chart.xScale = (d) => {
+        if (!d) return xScale;
+        xScale = d;
+        return chart;
+    };
+
+    chart.xDomain = (d) => {
+        if (typeof d === 'undefined') return xScale.domain();
+        xScale.domain(d);
+        return chart;
+    };
+
+    chart.xRange = (d) => {
+        if (typeof d === 'undefined') return xScale.range();
+        xScale.rangeRound(d);
+        return chart;
+    };
+
+    chart.plotDim = (d) => {
+        if (!d) return window.plotDim;
+        window.plotDim = d;
+        return chart;
+    };
+
+    chart.rem = (d) => {
+        if (!d) return rem;
+        rem = d;
+        return chart;
+    };
+
+    chart.includeMarker = (d) => {
+        if (typeof d === 'undefined') return includeMarker;
+        includeMarker = d;
+        return chart;
+    };
+
+    chart.markers = (d) => {
+        if (typeof d === 'undefined') return markers;
+        markers = d;
+        return chart;
+    };
+
+    chart.interpolation = (d) => {
+        if (!d) return interpolation;
+        interpolation = d;
+        return chart;
+    };
+
+    chart.colourPalette = (d) => {
+        if (!d) return colourScale;
+        if (d === 'social' || d === 'video') {
+            colourScale.range(gChartcolour.lineSocial);
+        } else if (d === 'webS' || d === 'webM' || d === 'webMDefault' || d === 'webL') {
+            colourScale.range(gChartcolour.categorical_bar);
+        } else if (d === 'print') {
+            colourScale.range(gChartcolour.barPrint);
+        }
+        return chart;
+    };
+
+    return chart;
+}
