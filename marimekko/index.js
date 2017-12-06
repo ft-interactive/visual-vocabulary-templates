a7032c9345a15360cf73f0d24a6d6f9f210760a6
Steve Bernard <steve.bernard@ft.com>
2017-12-06 14:26:44 +0000

    add left and right bar labels

--- marimekko/index.js
+++ marimekko/index.js
@@ -26,7 +26,7 @@ const sort = '';// specify 'ascending', 'descending', 'alphabetical' - default i
 const frame = {
     webS: gChartframe.webFrameS(sharedConfig)
         .margin({
-            top: 100, left: 15, bottom: 82, right: 20,
+            top: 100, left: 0, bottom: 82, right: 10,
         })
     // .title("Put headline here") //use this if you need to override the defaults
     // .subtitle("Put headline |here") //use this if you need to override the defaults
@@ -34,21 +34,21 @@ const frame = {
 
     webM: gChartframe.webFrameM(sharedConfig)
         .margin({
-            top: 100, left: 20, bottom: 86, right: 24,
+            top: 100, left: 0, bottom: 86, right: 10,
         })
     // .title("Put headline here")
         .height(500),
 
     webMDefault: gChartframe.webFrameMDefault(sharedConfig)
         .margin({
-            top: 100, left: 20, bottom: 86, right: 24,
+            top: 100, left: 0, bottom: 86, right: 10,
         })
     // .title("Put headline here")
         .height(500),
 
     webL: gChartframe.webFrameL(sharedConfig)
         .margin({
-            top: 100, left: 20, bottom: 104, right: 24,
+            top: 100, left: 0, bottom: 104, right: 10,
         })
     // .title("Put headline here")
         .height(700)
@@ -56,7 +56,7 @@ const frame = {
 
     print: gChartframe.printFrame(sharedConfig)
         .margin({
-            top: 40, left: 7, bottom: 35, right: 7,
+            top: 40, left: 5, bottom: 35, right: 5,
         })
     // .title("Put headline here")
         /* Print column widths */
@@ -94,7 +94,9 @@ d3.selectAll('.framed')
     });
 
 parseData.load(dataFile, { sort })
-    .then(({ valueExtent, plotData, seriesNames, totalSize }) => {
+    .then(({
+        valueExtent, plotData, seriesNames, totalSize,
+    }) => {
         Object.keys(frame).forEach((frameName) => {
             const currentFrame = frame[frameName];
 
@@ -102,15 +104,18 @@ parseData.load(dataFile, { sort })
             const myYAxis = gAxis.yLinear();
             const myChart = stackedBarChart.draw(); // eslint-disable-line
             const myLegend = gLegend.legend();
+            const formatNumber = d3.format(',');
 
             // define other functions to be called
-            const tickSize = currentFrame.dimension().height + (currentFrame.rem() * 1.4);// Used when drawing the xAxis ticks
+            const tickSize = currentFrame.dimension().height + (currentFrame.rem() * 2);// Used when drawing the xAxis ticks
 
             myYAxis
                 .align(yAxisAlign)
                 .domain([totalSize, 0])
                 .range([currentFrame.dimension().height, 0])
                 .frameName(frameName);
+
+            const yLabelScale = myYAxis.scale();
 
             myXAxis
                 .align(xAxisAlign)
@@ -124,19 +129,54 @@ parseData.load(dataFile, { sort })
             currentFrame.plot()
                 .call(myYAxis);
 
+
+            const leftLabel = currentFrame.plot().append('g').attr('class', 'axis yAxis');
+            const rightLabel = currentFrame.plot().append('g').attr('class', 'axis yAxis');
+            let labelWidthL = 0;
+            let labelWidthR = 0;
+
+            leftLabel
+                .selectAll('text')
+                .data(plotData)
+                .enter()
+                .append('text')
+                .attr('class', 'yLabel')
+                .text(d => d.name)
+                .attr('y', (d, i) => (yLabelScale(d.yPos) + Math.round(Number(i * (currentFrame.rem() / 10)))))
+                .attr('dy', d => (yLabelScale(d.size / 2) + (currentFrame.rem() / 4)))
+                .attr('x', -(currentFrame.rem() / 4));
+
+            rightLabel
+                .selectAll('text')
+                .data(plotData)
+                .enter()
+                .append('text')
+                .attr('class', 'yLabel')
+                .text(d => formatNumber(d.size))
+                .attr('y', (d, i) => (yLabelScale(d.yPos) + Math.round(Number(i * (currentFrame.rem() / 10)))))
+                .attr('dy', d => (yLabelScale(d.size / 2) + (currentFrame.rem() / 4)))
+                .attr('x', -(currentFrame.rem() / 4))
+                .style('text-anchor', 'start');
+
+            // Calculate width of widest left label text
+            leftLabel.selectAll('.yLabel').each(function calcTickTextWidthL() {
+                labelWidthL = Math.max(this.getBBox().width, labelWidthL);
+            });
+
+            // Calculate width of widest right label text
+            rightLabel.selectAll('.yLabel').each(function calcTickTextWidthR() {
+                labelWidthR = Math.max(this.getBBox().width, labelWidthR);
+            });
+
             // return the value in the variable newMargin
-            if (yAxisAlign === 'right') {
-                const newMargin = myYAxis.labelWidth() + currentFrame.margin().right;
-                // Use newMargin redefine the new margin and range of xAxis
-                currentFrame.margin({ right: newMargin });
-                myYAxis.yLabel().attr('transform', `translate(${currentFrame.dimension().width},0)`);
-            }
-            if (yAxisAlign === 'left') {
-                const newMargin = myYAxis.labelWidth() + currentFrame.margin().left;
-                // Use newMargin redefine the new margin and range of xAxis
-                currentFrame.margin({ left: newMargin });
-            // myYAxis.yLabel().attr('transform', `translate(${(myYAxis.tickSize()-myYAxis.labelWidth())},0)`);
-            }
+            const newMarginR = labelWidthR + currentFrame.margin().right;
+            // Use newMargin redefine the new margin and range of xAxis
+            currentFrame.margin({ right: newMarginR });
+            rightLabel.attr('transform', `translate(${currentFrame.dimension().width - labelWidthL},0)`);
+
+            const newMarginL = labelWidthL + currentFrame.margin().left + (currentFrame.rem() / 2);
+            // Use newMargin redefine the new margin and range of xAxis
+            currentFrame.margin({ left: newMarginL });
 
             d3.select(currentFrame.plot().node().parentNode)
                 .call(currentFrame);
@@ -152,7 +192,7 @@ parseData.load(dataFile, { sort })
                 myXAxis.xLabel().attr('transform', `translate(0,${currentFrame.dimension().height})`);
             }
             if (xAxisAlign === 'top') {
-                myXAxis.xLabel().attr('transform', `translate(0,${myXAxis.tickSize()})`);
+                myXAxis.xLabel().attr('transform', `translate(0,${myXAxis.tickSize() - (currentFrame.rem() / 4)})`);
             }
 
             myChart
@@ -192,6 +232,7 @@ parseData.load(dataFile, { sort })
                 .call(myLegend);
 
             myYAxis.yLabel().remove();
+
         });
     // addSVGSavers('figure.saveable');
     });
