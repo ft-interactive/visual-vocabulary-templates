ce1655968e10961e0d269bc205bb4961b9a75557
Cale Tilford <cale.tilford@ft.com>
2018-01-09 18:15:45 +0000

    Use calendar heatmap as starting point

---
+++ calendar-heatmap-vertical/calendarHeatmap.js
@@ -0,0 +1,175 @@
+import * as d3 from 'd3';
+// import gChartcolour from 'g-chartcolour';
+
+export function draw() {
+    let rem = 16;
+    let fiscal = false;
+    let scaleBreaks = [];
+    let colourPalette = [];
+    let cellSize = 10;
+    let clipYear = true;
+    const cScale = d3.scaleThreshold()
+        .domain(scaleBreaks)
+        .range(colourPalette);
+
+    function chart(parent) {
+        parent
+            .attr('id', d => `calendar_${d.key}`)
+            .attr('transform', (d, i) => {
+                const calendarOffset = (i * ((cellSize * 7) + (rem * 2))) - (rem * 1.3);
+                return `translate(${calendarOffset}, 0)`;
+            });
+
+        // Add days
+        parent
+            .append('g')
+            .attr('transform', `translate(0,${rem * 1.5})`)
+            .attr('class', 'dayHolder')
+            .selectAll('.days')
+            .data(d => d.values)
+            .enter()
+            .append('rect')
+            .attr('id', d => `date: ${d.date} value: ${d.value}`)
+            .attr('class', 'days')
+            .attr('width', cellSize)
+            .attr('height', cellSize)
+            .attr('y', (d) => {
+                if (fiscal) {
+                    return d.fweek * cellSize;
+                }
+                return getWeekOfYear(d.date) * cellSize;
+            })
+            .attr('x', d => +d.date.getDay() * cellSize)
+            .style('fill', d => cScale(d.value));
+
+        parent
+            .append('g')
+            .attr('class', 'monthOutlines')
+            .selectAll('.months')
+            .data((d) => {
+                if (fiscal) {
+                    return d3.timeMonths(
+                        new Date(parseInt(d.key, 10) - 1, 3, 1),
+                        new Date(parseInt(d.key, 10), 2, 31),
+                    );
+                }
+                return d3.timeMonths(
+                    new Date(parseInt(d.key, 10), 0, 1),
+                    new Date(parseInt(d.key, 10), 11, 31),
+                );
+            })
+            .enter()
+            .append('path')
+            .attr('class', 'months')
+            .attr('transform', `translate(0, ${rem * 1.5})`)
+            .attr('d', d => monthPath(d, fiscal, cellSize, clipYear));
+    }
+
+    chart.plotDim = (d) => {
+        if (!d) return window.plotDim;
+        window.plotDim = d;
+        return chart;
+    };
+
+    chart.rem = (d) => {
+        if (!d) return rem;
+        rem = d;
+        return chart;
+    };
+
+    chart.cellSize = (d) => {
+        if (!d) return cellSize;
+        cellSize = d;
+        return chart;
+    };
+
+    chart.clipYear = (d) => {
+        if (typeof d === 'undefined') return clipYear;
+        clipYear = d;
+        return chart;
+    };
+
+    chart.fiscal = (d) => {
+        if (typeof d === 'undefined') return fiscal;
+        fiscal = d;
+        return chart;
+    };
+
+    chart.scaleBreaks = (d) => {
+        if (typeof d === 'undefined') return scaleBreaks;
+        scaleBreaks = d;
+        cScale.domain(d);
+        return chart;
+    };
+
+    chart.colourPalette = (d) => {
+        if (!d) return colourPalette;
+        colourPalette = d;
+        cScale.range(d);
+        return chart;
+    };
+
+    return chart;
+}
+
+function monthPath(t0, fiscal, cellSize, clipYear) {
+    const t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0);
+    let w0;
+    let w1;
+
+    if (fiscal) {
+        w0 = +getFiscalWeek(t0);
+        w1 = +getFiscalWeek(t1);
+        if (w0 > w1) { w0 = 0; }
+    } else {
+        w0 = +getWeekOfYear(t0);
+        w1 = +getWeekOfYear(t1);
+    }
+
+    let d0 = +t0.getDay();
+    const d1 = +t1.getDay();
+
+    if (w0 === 0 && !clipYear) {
+        d0 = 0;
+    }
+
+    w0 = parseInt(w0, 10);
+    w1 = parseInt(w1, 10);
+
+    // return `M${(d0 + 1) * cellSize},${w0 * cellSize}`
+    //     + `V${w0 * cellSize}H${7 * cellSize}`
+    //     + `V${w1 * cellSize}H${(d1 + 1) * cellSize}`
+    //     + `V${(w1 + 1) * cellSize}H0`
+    //     + `V${(w0 + 1) * cellSize}Z`;
+    return `M${(d0 + 1) * cellSize},${w0 * cellSize}`
+        + `V${w0 * cellSize}H${7 * cellSize}`
+        + `V${w1 * cellSize}H${(d1 + 1) * cellSize}`
+        + `V${(w1 + 1) * cellSize}H0`
+        + `V${(w0 + 1) * cellSize}Z`;
+    // return `M${(w0 + 1) * cellSize},${d0 * cellSize}`
+    //     + `H${w0 * cellSize}V${7 * cellSize}`
+    //     + `H${w1 * cellSize}V${(d1 + 1) * cellSize}`
+    //     + `H${(w1 + 1) * cellSize}V0`
+    //     + `H${(w0 + 1) * cellSize}Z`;
+}
+
+function getWeekOfYear(e) {
+    return d3.timeFormat('%U')(e);
+}
+
+function getFiscalWeek(e) {
+    // will need to refactor this to somewhere cellSize
+    const parseDate = d3.timeParse('%d/%m/%Y');
+
+    const startDate = `06/04/${e.getFullYear()}`;
+    const week = getWeekOfYear(e);
+    const startWeek = getWeekOfYear(parseDate(startDate));
+
+    let fweek;
+    if (e >= parseDate(startDate)) {
+        fweek = week - startWeek;
+    } else {
+        fweek = 52 - (startWeek - week);
+    }
+    return fweek;
+}
---
+++ calendar-heatmap-vertical/index.js
@@ -0,0 +1,281 @@
+import * as d3 from 'd3';
+import gChartframe from 'g-chartframe';
+import gChartcolour from 'g-chartcolour';
+import * as gLegend from 'g-legend';
+// import * as gAxis from 'g-axis';
+import * as parseData from './parseData.js';
+import * as calendarHeatmap from './calendarHeatmap.js';
+
+const dataFile = 'data.csv';
+
+const dateFormat = '%d/%m/%Y';
+/*
+  some common formatting parsers....
+  '%m/%d/%Y'        01/28/1986
+  '%d-%b-%y'        28-Jan-86
+  '%Y %b'           1986 Jan
+  '%Y-%m-%d'        1986-01-28
+  '%B %d'           January 28
+  '%d %b'           28 Jan
+  '%H:%M'           11:39
+  '%H:%M %p'        11:39 AM
+  '%d/%m/%Y %H:%M'  28/01/2016 11:39
+*/
+
+const sharedConfig = {
+    title: 'Title not yet added',
+    subtitle: 'Subtitle not yet added',
+    source: 'Source not yet added',
+};
+
+const legendAlign = 'hori';// hori or vert, alignment of the legend
+const legendType = 'rect'; // rect, line or circ, geometry of legend marker
+
+const fiscal = false;// should be true if you want to disply financial years
+const clipYear = true;// if true clip to the year otherwise the front of the year is flat
+const scaleBreaks = [20, 40, 60, 80, 100];// set breaks for the colour scale
+const scaleType = 'sequentialRed';// set the colour scale to 'sequentialBlue' or 'sequentialRed'
+let colourRange;
+
+// Individual frame configuratiuon, used to set margins (defaults shown below) etc
+const frame = {
+    webS: gChartframe.webFrameS(sharedConfig)
+        .margin({
+            top: 100, left: 0, bottom: 82, right: 0,
+        })
+    // .title("Put headline here") //use this if you need to override the defaults
+    // .subtitle("Put headline |here") //use this if you need to override the defaults
+        .height(400),
+
+    webM: gChartframe.webFrameM(sharedConfig)
+        .margin({
+            top: 100, left: 0, bottom: 86, right: 0,
+        })
+    // .title("Put headline here")
+        .height(500),
+
+    webMDefault: gChartframe.webFrameMDefault(sharedConfig)
+        .margin({
+            top: 100, left: 0, bottom: 86, right: 0,
+        })
+    // .title("Put headline here")
+        .height(1000),
+
+    webL: gChartframe.webFrameL(sharedConfig)
+        .margin({
+            top: 100, left: 0, bottom: 104, right: 0,
+        })
+    // .title("Put headline here")
+        .height(700)
+        .fullYear(true),
+
+    print: gChartframe.printFrame(sharedConfig)
+        .margin({
+            top: 40, left: 0, bottom: 35, right: 0,
+        })
+    // .title("Put headline here")
+    // .width(53.71)// 1 col
+    // .width(112.25)// 2 col
+    .width(170.8)// 3 col
+        // .width(229.34)// 4 col
+    // .width(287.88)// 5 col
+    // .width(346.43)// 6 col
+    // .width(74)// markets std print
+    // .height(69.85), //  std print (Use 58.21mm for markets charts that matter)
+        .height(100),
+
+    // social: gChartframe.socialFrame(sharedConfig)
+    //     .margin({
+    //         top: 140, left: 50, bottom: 138, right: 40,
+    //     })
+    // // .title("Put headline here")
+    //     .width(612)
+    //     .height(612),
+
+    // video: gChartframe.videoFrame(sharedConfig)
+    //     .margin({
+    //         left: 207, right: 207, bottom: 210, top: 233,
+    //     }),
+    // // .title("Put headline here")
+};
+
+
+// add the frames to the page...
+d3.selectAll('.framed')
+    .each(function addFrames() {
+        const figure = d3.select(this);
+        figure.select('svg')
+            .call(frame[figure.node().dataset.frame]);
+    });
+
+parseData.load(dataFile, { fiscal, dateFormat })
+    .then(({
+        plotData, data, // eslint-disable-line no-unused-vars
+    }) => {
+        Object.keys(frame).forEach((frameName) => {
+            const currentFrame = frame[frameName];
+
+            const myChart = calendarHeatmap.draw();
+            const myLegend = gLegend.legend();
+
+            switch (scaleType) {
+            case 'sequentialRed':
+                colourRange = gChartcolour.sequentialMulti_2;
+                break;
+
+            case 'sequentialBlue':
+                colourRange = gChartcolour.sequentialMulti;
+                break;
+
+            default:
+            }
+
+            const colourScale = d3.scaleOrdinal()
+                .domain(scaleBreaks)
+                .range(colourRange);
+
+            const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+            const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+            myChart
+                .fiscal(fiscal)
+                .clipYear(clipYear)
+                .plotDim(currentFrame.dimension())
+                .rem(currentFrame.rem())
+                .scaleBreaks(scaleBreaks)
+                .colourPalette(colourRange);
+
+            const years = currentFrame.plot()
+                .selectAll('.year')
+                .data(plotData)
+                .enter()
+                .append('g')
+                .attr('class', 'year');
+
+            // Append label for each year
+            const yearLabels = years.append('text')
+                .attr('class', 'year-labels')
+                .attr('y', currentFrame.rem())
+                .text((d) => {
+                    if (fiscal) {
+                        return `Year ending ${d.key}`;
+                    }
+                    return d.key;
+                });
+
+            // We're not using an axis so need to caluclate max label width
+            let labelWidth = 0;
+            yearLabels.each(function getMaxLabelWidth() {
+                labelWidth = Math.max(this.getBBox().width, labelWidth);
+            });
+            // Add option to specify where labels are
+            const newMargin = (labelWidth * 1.2) + currentFrame.margin().left;
+            currentFrame.margin({ left: newMargin });
+
+            const newRightMargin = (labelWidth * 1.2) + currentFrame.margin().right;
+            currentFrame.margin({ right: newRightMargin });
+            yearLabels.attr('transform', `translate(${-labelWidth * 1.2},0)`);
+
+            d3.select(currentFrame.plot().node().parentNode)
+                .call(currentFrame);
+
+            // Calculate cell size and pass this to the chart
+            const cellSize = Math.min(currentFrame.dimension().width / 53, currentFrame.dimension().height / 24);
+            myChart.cellSize(cellSize);
+
+            years.call(myChart);
+
+            const dayLabelsLeft = years
+                .append('g')
+                .attr('class', 'dayLabelsLeft');
+            const dayLabelsRight = years
+                .append('g')
+                .attr('class', 'dayLabelsRight');
+
+            days.forEach((day, i) => {
+                dayLabelsLeft.append('text')
+                    .attr('class', 'weekday-labels')
+                    .attr('y', (currentFrame.rem() * 1.4) + (i * cellSize))
+                    .attr('dy', '0.9em')
+                    .attr('text-anchor', 'start')
+                    .text(day);
+                dayLabelsRight.append('text')
+                    .attr('class', 'weekday-labels')
+                    .attr('y', (currentFrame.rem() * 1.4) + (i * cellSize))
+                    .attr('dy', '0.9em')
+                    .attr('text-anchor', 'end')
+                    .text(day);
+            });
+
+            let dayLabelsWidth = 0;
+            dayLabelsLeft.each(function getMaxLabelWidth() {
+                dayLabelsWidth = Math.max(this.getBBox().width, dayLabelsWidth);
+            });
+
+            dayLabelsLeft.attr('transform', `translate(${-labelWidth * 1.2},0)`);
+            dayLabelsRight.attr('transform', `translate(${currentFrame.dimension().width + (labelWidth * 1.2)},0)`);
+
+            d3.select(currentFrame.plot().node().parentNode)
+                .call(currentFrame);
+
+            // Get the bounding boxes of month outlines
+            const boundingBoxes = [];
+            const mp = currentFrame.plot()
+                .select('.monthOutlines')
+                .node()
+                .childNodes;
+            for (let i = 0; i < mp.length; i += 1) {
+                boundingBoxes.push(mp[i].getBBox());
+            }
+            const monthX = [];
+            boundingBoxes.forEach((d) => {
+                const boxCentre = d.width / 2;
+                monthX.push(d.x + boxCentre);
+            });
+
+            const monthLabels = years
+                .append('g')
+                .attr('class', 'monthLabels');
+
+            months.forEach((d, i) => {
+                monthLabels.append('text')
+                    .attr('class', 'month-labels')
+                    .attr('x', () => {
+                        if (fiscal && i < 3) {
+                            return monthX[i + 9];
+                        }
+                        if (fiscal && i > 2) {
+                            return monthX[i - 3];
+                        }
+                        return monthX[i];
+                    })
+                    .attr('y', myChart.rem())
+                    .text(d);
+            });
+
+            myLegend
+                .seriesNames(scaleBreaks)
+                .geometry(legendType)
+                .frameName(frameName)
+                .rem(myChart.rem())
+                .alignment(legendAlign)
+                .colourPalette(colourScale);
+
+            // Draw the Legend
+            currentFrame.plot()
+                .append('g')
+                .attr('id', 'legend')
+                .selectAll('.legend')
+                .data(scaleBreaks)
+                .enter()
+                .append('g')
+                .classed('legend', true)
+                .call(myLegend);
+
+            const legendSelection = currentFrame.plot().select('#legend');
+            const legendWidth = (legendSelection.node().getBBox().width); // eslint-disable-line
+            legendSelection.attr('transform', `translate(${currentFrame.dimension().width - legendWidth - currentFrame.rem()},${-currentFrame.rem() * 2})`);
+        });
+
+    // addSVGSavers('figure.saveable');
+    });
