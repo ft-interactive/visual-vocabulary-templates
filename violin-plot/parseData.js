435795627fa9247508431f0a7640ab3df641d06c
Cale Tilford <cale.tilford@ft.com>
2018-07-26 11:39:24 +0100

    Add quartile calculation

--- violin-plot/parseData.js
+++ violin-plot/parseData.js
@@ -16,15 +16,32 @@ export function load(url, options) {
     // eslint-disable-line
     return loadData(url).then(result => {
         const data = result.data ? result.data : result;
-        data.forEach(d => (d.value = +d.value));
+        data.forEach(d => (d.value = Number(d.value)));
 
         // Get group names
-        const groupNames = data.map(d => d.group);
+        const groupNames = data
+            .map(d => d.group)
+            .filter(
+                (item, pos, groupNames) => groupNames.indexOf(item) === pos
+            );
 
         // Find the min and max values in the data set
         const valueExtent = d3.extent(data, d => d.value);
 
-        const plotData = [];
+        const plotData = groupNames.map((d, i) => {
+            let groupValues = data.filter(el => {
+                return el.group === d;
+            });
+            let qValues = groupValues.map(d => d.value);
+            qValues = qValues.sort((a, b) => a - b);
+            return {
+                group: d,
+                values: qValues,
+                q1: d3.quantile(qValues, 0.25),
+                q2: d3.quantile(qValues, 0.5),
+                q3: d3.quantile(qValues, 0.75)
+            };
+        });
 
         return {
             valueExtent,
