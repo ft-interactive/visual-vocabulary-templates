5e2858ebb628401f4f696a214fc23839d5bfe155
Bob Haslett <bob.haslett@ft.com>
2018-07-30 13:56:26 +0100

    Update values definition in parse`Data on Scatterplot

--- scatter/parseData.js
+++ scatter/parseData.js
@@ -13,10 +13,12 @@ import loadData from '@financial-times/load-data';
 export function load(url, options) { // eslint-disable-line
     return loadData(url).then((result) => {
         const data = result.data ? result.data : result;
+        const {xVar, yVar, sizeVar} = options;
+        console.log(xVar, yVar, sizeVar)
 
-        let sizeVar = options.sizeVar;
-        let xVar = options.xVar;
-        let yVar = options.yVar;
+        // let sizeVar = options.sizeVar;
+        // let xVar = options.xVar;
+        // let yVar = options.yVar;
 
         // automatically calculate the seriesnames excluding the reserved "name" and "group" fields
         const seriesNames = getSeriesNames(data.columns);
