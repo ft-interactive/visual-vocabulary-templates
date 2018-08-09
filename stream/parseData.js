c19521fdcfbc5ade2902e43edbcf2c6b194294e0
Bob Haslett <bob.haslett@ft.com>
2018-08-09 14:21:59 +0100

    Add stream chart template

---
+++ stream/parseData.js
@@ -0,0 +1,164 @@
+/**
+ * General data munging functionality
+ */
+
+import * as d3 from 'd3';
+import loadData from '@financial-times/load-data';
+
+/**
+ * Parses data file and returns structured data
+ * @param  {String} url Path to CSV/TSV/JSON file
+ * @return {Object}     Object containing series names, value extent and raw data object
+ */
+export function load(url, options) { // eslint-disable-line
+    return loadData(url).then((result) => {
+        const data = result.data ? result.data : result;
+        const { dateFormat, yMin, joinPoints, highlightNames } = options; // eslint-disable-line no-unused-vars
+        // make sure all the dates in the date column are a date object
+        const parseDate = d3.timeParse(dateFormat);
+        data.forEach((d) => {
+            d.date = parseDate(d.date);
+        });
+
+        // Automatically calculate the seriesnames excluding the "marker" and "annotate column"
+        let seriesNames = getSeriesNames(data.columns);
+        // Use the seriesNames array to calculate the minimum and max values in the dataset
+        const valueExtent = extentMulti(data, seriesNames, yMin);
+
+        let stack = d3.stack()
+            .keys(seriesNames)
+            .order(d3.stackOrderNone)
+            .offset(d3.stackOffsetWiggle);
+
+        var series = stack(data);
+
+        series.forEach((d, i) => {
+            valueExtent[0] = Math.min(valueExtent[0], d3.min(d.map((el) => el[0])));
+            valueExtent[1] = Math.max(valueExtent[1], d3.max(d.map((el) => el[1])))
+        })
+
+        // let min = series.map((d) => {return d3.min(d[0])})
+        // let max = series.map((d) => {return d3.max(d[1])})
+
+        //console.log('test', min, max)
+
+        const isLineHighlighted = (el) => highlightNames.some(d => d === el);
+
+        // Format the dataset that is used to draw the lines
+        let highlightLines = {};
+        let plotData = seriesNames.map(d => ({
+            name: d,
+            lineData: getlines(data, d),
+            highlightLine: isLineHighlighted(d),
+        }));
+
+        highlightLines = plotData.filter(d => d.highlightLine === true);
+        plotData = plotData.filter(d => d.highlightLine === false);
+
+         // Filter data for annotations
+        const annos = data.filter(d => (d.annotate !== '' && d.annotate !== undefined));
+
+        // Format the data that is used to draw highlight tonal bands
+        const boundaries = data.filter(d => (d.highlight === 'begin' || d.highlight === 'end'));
+        const highlights = [];
+
+        boundaries.forEach((d, i) => {
+            if (d.highlight === 'begin') {
+                highlights.push({ begin: d.date, end: boundaries[i + 1].date });
+            }
+        });
+
+        return {
+            seriesNames,
+            data,
+            plotData,
+            highlightLines,
+            valueExtent,
+            highlights,
+            annos,
+            series,
+        };
+    });
+}
+
+
+/**
+ * Returns the columns headers from the top of the dataset, excluding specified
+ * @param  {[type]} columns [description]
+ * @return {[type]}         [description]
+ */
+export function getSeriesNames(columns) {
+    const exclude = ['date', 'annotate', 'highlight'];
+    return columns.filter(d => (exclude.indexOf(d) === -1));
+}
+
+/**
+ * Calculates the extent of multiple columns
+ * @param  {[type]} d       [description]
+ * @param  {[type]} columns [description]
+ * @param  {[type]} yMin    [description]
+ * @return {[type]}         [description]
+ */
+export function extentMulti(d, columns, yMin) {
+    const ext = d.reduce((acc, row) => {
+        const values = columns.map(key => row[key])
+        .map((item) => {
+            if (!item || item === '*') {
+                return yMin;
+            }
+            return Number(item);
+        });
+        const rowExtent = d3.extent(values);
+        if (!acc.max) {
+            acc.max = rowExtent[1];
+            acc.min = rowExtent[0];
+        } else {
+            acc.max = Math.max(acc.max, rowExtent[1]);
+            acc.min = Math.min(acc.min, rowExtent[0]);
+        }
+        return acc;
+    }, {});
+    return [ext.min, ext.max];
+}
+
+/**
+ * Sorts the column information in the dataset into groups according to the column
+ * head, so that the line path can be passed as one object to the drawing function
+ */
+export function getlines(d, group) {
+    // console.log('d and group',d,group)
+    const lineData = [];
+    d.forEach((el) => {
+        // console.log(el,i)
+        const column = {};
+        column.name = group;
+        column.date = el.date;
+        column.value = +el[group];
+        column.highlight = el.highlight;
+        column.annotate = el.annotate;
+        if (el[group]) {
+            lineData.push(column);
+        }
+
+        // if(el[group] == false) {
+        //     lineData.push(null)
+        // }
+        if (el[group] === false && joinPoints === false) {
+            lineData.push(null);
+        }
+    });
+    return lineData;
+    // return d.map((el) => {
+    //     if (el[group]) {
+    //         return {
+    //             name: group,
+    //             date: el.date,
+    //             value: +el[group],
+    //             highlight: el.highlight,
+    //             annotate: el.annotate,
+    //         };
+    //     }
+
+    //     return null;
+    // }).filter(i => i);
+}
---
+++ stream/streamChart.js
@@ -0,0 +1,296 @@
+import * as d3 from 'd3';
+import gChartcolour from 'g-chartcolour';
+
+let rem = 10;
+
+export function draw() {
+    let yScale = d3.scaleLinear();
+    let xScale = d3.scaleTime();
+    let seriesNames = [];
+    let highlightNames = [];
+    let yAxisAlign = 'right';
+    let markers = false;
+    const includeAnnotations = d => (d.annotate !== '' && d.annotate !== undefined); // eslint-disable-line
+    let annotate = false; // eslint-disable-line
+    let interpolation = d3.curveLinear;
+    let colourScale = d3.scaleOrdinal()
+    // .range(gChartcolour.lineWeb)
+    .domain(seriesNames);
+
+    function chart(parent) {
+
+        const colour = d3.scaleOrdinal(d3.schemeCategory20);
+
+
+        var area = d3.area()
+            .x(function(d) {return xScale(d.data.date)})
+            .y0(function(d) { return Number(yScale(d[0]))})
+            .y1(function(d) { return Number(yScale(d[1]))})
+            .curve(d3.curveBasis);
+
+        parent.append("path")
+        .attr('id', d => d.key)
+        .attr("d", area)
+        //.style('fill', d => colour(d.key));
+        .style('fill', d => colourScale(d.key));
+    }
+
+    chart.yScale = (d) => {
+        if (!d) return yScale;
+        yScale = d;
+        return chart;
+    };
+
+    chart.yAxisAlign = (d) => {
+        if (!d) return yAxisAlign;
+        yAxisAlign = d;
+        return chart;
+    };
+
+    chart.yDomain = (d) => {
+        if (typeof d === 'undefined') return yScale.domain();
+        yScale.domain(d);
+        return chart;
+    };
+
+    chart.yRange = (d) => {
+        if (typeof d === 'undefined') return yScale.range();
+        yScale.range(d);
+        return chart;
+    };
+
+    chart.highlightNames = (d) => {
+        if (!d) return highlightNames;
+        highlightNames = d;
+        return chart;
+    };
+
+    chart.seriesNames = (d) => {
+        if (typeof d === 'undefined') return seriesNames;
+        seriesNames = d;
+        return chart;
+    };
+
+    chart.xScale = (d) => {
+        if (!d) return xScale;
+        xScale = d;
+        return chart;
+    };
+
+    chart.xDomain = (d) => {
+        if (typeof d === 'undefined') return xScale.domain();
+        xScale.domain(d);
+        return chart;
+    };
+
+    chart.xRange = (d) => {
+        if (typeof d === 'undefined') return xScale.range();
+        xScale.range(d);
+        return chart;
+    };
+
+    chart.plotDim = (d) => {
+        if (!d) return window.plotDim;
+        window.plotDim = d;
+        return chart;
+    };
+
+    chart.rem = (d) => {
+        if (!d) return rem;
+        rem = d;
+        return chart;
+    };
+
+    chart.annotate = (d) => {
+        annotate = d;
+        return chart;
+    };
+
+    chart.markers = (d) => {
+        if (typeof d === 'undefined') return markers;
+        markers = d;
+        return chart;
+    };
+
+    chart.interpolation = (d) => {
+        if (!d) return interpolation;
+        interpolation = d;
+        return chart;
+    };
+
+    chart.colourPalette = (d) => {
+        if (!d) return colourScale;
+        if (highlightNames.length > 0) {
+            if (d === 'social' || d === 'video') {
+                colourScale.range(gChartcolour.mutedFirstLineSocial);
+            } else if (d === 'webS' || d === 'webM' || d === 'webMDefault' || d === 'webL') {
+                colourScale.range(gChartcolour.mutedFirstLineWeb);
+            } else if (d === 'print') {
+                colourScale.range(gChartcolour.mutedFirstLinePrint);
+            } else if (d && d.name && d.name === 'scale') {
+                colourScale = d;
+            }
+            return chart;
+        }
+        if (d === 'social' || d === 'video') {
+            colourScale.range(gChartcolour.lineSocial);
+        } else if (d === 'webS' || d === 'webM' || d === 'webMDefault' || d === 'webL') {
+            colourScale.range(gChartcolour.lineWeb);
+        } else if (d === 'print') {
+            colourScale.range(gChartcolour.linePrint);
+        }
+        return chart;
+    };
+
+    return chart;
+}
+
+export function label() {
+    let yScale = d3.scaleLinear();
+    let xScale = d3.scaleTime();
+    let seriesNames = [];
+    let highlightNames = [];
+    let yAxisAlign = 'right';
+    let markers = false;
+    const includeAnnotations = d => (d.annotate !== '' && d.annotate !== undefined); // eslint-disable-line
+    let annotate = false; // eslint-disable-line
+    let interpolation = d3.curveLinear;
+    let colourScale = d3.scaleOrdinal()
+    // .range(gChartcolour.lineWeb)
+    .domain(seriesNames);
+
+    function chart(parent) {
+
+        parent.append("text")
+            .attr('class', 'annotation')
+            .attr('text-anchor', 'middle')
+            .attr('x', d => xScale(getPos(d)[0]))
+            .attr('y', d => yScale(getPos(d)[1]))
+            .text(d => d.key);
+
+        function getPos(d) {
+            let x = ''
+            let y = 0
+            let value = 0
+            d.forEach((el) => {
+                if (Number(el.data[d.key]) > value) {
+                    value = el.data[d.key]
+                    x = el.data.date;
+                    y = Number(el[0] + ((el[1] - el[0])/2))
+                }
+            })
+            return [x, y]
+        }
+
+    }
+
+    chart.yScale = (d) => {
+        if (!d) return yScale;
+        yScale = d;
+        return chart;
+    };
+
+    chart.yAxisAlign = (d) => {
+        if (!d) return yAxisAlign;
+        yAxisAlign = d;
+        return chart;
+    };
+
+    chart.yDomain = (d) => {
+        if (typeof d === 'undefined') return yScale.domain();
+        yScale.domain(d);
+        return chart;
+    };
+
+    chart.yRange = (d) => {
+        if (typeof d === 'undefined') return yScale.range();
+        yScale.range(d);
+        return chart;
+    };
+
+    chart.highlightNames = (d) => {
+        if (!d) return highlightNames;
+        highlightNames = d;
+        return chart;
+    };
+
+    chart.seriesNames = (d) => {
+        if (typeof d === 'undefined') return seriesNames;
+        seriesNames = d;
+        return chart;
+    };
+
+    chart.xScale = (d) => {
+        if (!d) return xScale;
+        xScale = d;
+        return chart;
+    };
+
+    chart.xDomain = (d) => {
+        if (typeof d === 'undefined') return xScale.domain();
+        xScale.domain(d);
+        return chart;
+    };
+
+    chart.xRange = (d) => {
+        if (typeof d === 'undefined') return xScale.range();
+        xScale.range(d);
+        return chart;
+    };
+
+    chart.plotDim = (d) => {
+        if (!d) return window.plotDim;
+        window.plotDim = d;
+        return chart;
+    };
+
+    chart.rem = (d) => {
+        if (!d) return rem;
+        rem = d;
+        return chart;
+    };
+
+    chart.annotate = (d) => {
+        annotate = d;
+        return chart;
+    };
+
+    chart.markers = (d) => {
+        if (typeof d === 'undefined') return markers;
+        markers = d;
+        return chart;
+    };
+
+    chart.interpolation = (d) => {
+        if (!d) return interpolation;
+        interpolation = d;
+        return chart;
+    };
+
+    chart.colourPalette = (d) => {
+        if (!d) return colourScale;
+        if (highlightNames.length > 0) {
+            if (d === 'social' || d === 'video') {
+                colourScale.range(gChartcolour.mutedFirstLineSocial);
+            } else if (d === 'webS' || d === 'webM' || d === 'webMDefault' || d === 'webL') {
+                colourScale.range(gChartcolour.mutedFirstLineWeb);
+            } else if (d === 'print') {
+                colourScale.range(gChartcolour.mutedFirstLinePrint);
+            } else if (d && d.name && d.name === 'scale') {
+                colourScale = d;
+            }
+            return chart;
+        }
+        if (d === 'social' || d === 'video') {
+            colourScale.range(gChartcolour.lineSocial);
+        } else if (d === 'webS' || d === 'webM' || d === 'webMDefault' || d === 'webL') {
+            colourScale.range(gChartcolour.lineWeb);
+        } else if (d === 'print') {
+            colourScale.range(gChartcolour.linePrint);
+        }
+        return chart;
+    };
+
+    return chart;
+}
+
