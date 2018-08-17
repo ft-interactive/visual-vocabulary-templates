f0b20e3c93a27ac2a51ef1098fd9622c483ec8b5
Bob Haslett <bob.haslett@ft.com>
2018-08-17 14:54:48 +0100

    Add g-annotations and define const

--- candlestick/index.js
+++ candlestick/index.js
@@ -9,6 +9,7 @@ import * as gAxis from 'g-axis';
 import gChartcolour from 'g-chartcolour';
 import * as parseData from './parseData.js';
 import * as candlestick from './candlestick.js';
+import * as annotation from 'g-annotations';
 
 const dataFile = 'data.csv';
 
@@ -132,12 +133,11 @@ parseData.load(dataFile, { dateFormat, yMin, highlightNames }).then(({
         // define other functions to be called
         const myYAxis = gAxis.yLinear();// sets up yAxis
         const myXAxis = gAxis.xDate();// sets up xAxis
+        const myAnnotations = annotation.annotations();// sets up annotations
         // const plotDim=currentFrame.dimension()//useful variable to carry the current frame dimensions
         const tickSize = currentFrame.dimension().width;// Used when drawing the yAxis ticks
         const myChart = candlestick.draw();
         const myHighlights = candlestick.drawHighlights();// sets up highlight tonal bands
-        const myAnnotations = candlestick.drawAnnotations();// sets up annotations
-
 
         // .seriesNames(seriesNames)
         // .highlightNames(highlightNames)
