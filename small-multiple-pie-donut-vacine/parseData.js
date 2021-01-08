c94186b800f17dcfe9a49710c4ef72f9a9304443
Bob Haslett <bob.haslett@ft.com>
2021-01-08 15:52:45 +0000

    Add files for first commit

---
+++ small-multiple-pie-donut-vacine/parseData.js
@@ -0,0 +1,107 @@
+/**
+ * General data munging functionality
+ */
+
+import * as d3 from 'd3';
+import loadData from '@financial-times/load-data';
+
+/**
+ * Parses data file and returns structured data
+ * @param  {String} url Path to CSV/TSV/JSON file
+ * @return {Object}     Object containing series names, value extent and raw data object
+ */
+export function load(url, options) { // eslint-disable-line
+    return loadData(url).then((result) => {
+        const data = result.data ? result.data : result;
+        const {dateFormat, countries, dateRange} = options;
+        const parseDate = d3.timeParse(dateFormat);
+        data.forEach((d) => {
+            d.date = parseDate(d.date);
+            d.area = d.area === 'United States' ?'US'
+            : d.area === 'United Kingdom' ?'UK'
+            : d.area
+        });
+        const tickRange = dateRange.map((d) => {
+            return parseDate(d);
+        });
+
+        const frameTimes = tickRange.length > 1 ? getTicks(tickRange)
+        : tickRange
+        function getTicks(dates) {
+            let newRange = d3.timeDay.every(1).range(dates[0], dates[1])
+            newRange.push(dates[1]) 
+            return newRange  
+        }
+        console.log(frameTimes)
+        // automatically calculate the seriesnames excluding the "marker" and "annotate column"
+        const seriesNames = countries[0] === 'All' ?  getSeriesNames(data) : countries
+        console.log('seriesNames', seriesNames)
+        // identify total size - used for y axis
+
+        const plotData = seriesNames.map(d => ({
+            code: d,
+            chartData: getPieData(data, d)
+        }));
+
+        console.log('plotData', plotData)
+        
+        function getPieData(data, key) {
+            let filteredData = data.filter(d => d.code === key)
+            .sort((a, b) => b.date - a.date)
+            .map((el) => ({
+                area: el.area,
+                code: el.code,
+                region: el.region,
+                continent: el.continent,
+                population: el.population,
+                date: el.date,
+                vaccinations: el.vaccinations,
+                vaccinated_percent: el.vaccinated_percent,
+                vaccinations_new: el.vaccinations_new,
+                vaccinations_new_7ma: el.vaccinations_new_7ma,
+                vaccinations_new_7ma_perM: el.vaccinations_new_7ma_perM,
+                source: el.source,
+                last_updated: el.last_updated,
+                values: [
+                    {name: 'Vacinated', value: Number(el.vaccinated_percent)},
+                    {name: 'Not vcinated', value: Number(100 - el.vaccinated_percent)},
+                ]
+        
+            }));
+            return filteredData
+
+        }
+        
+
+        return {
+            seriesNames,
+            data,
+            plotData,
+            frameTimes,
+        };
+    });
+}
+
+// a function that returns the columns headers from the top of the dataset, excluding specified
+function getSeriesNames(data) {
+    const series = data.map( d => d.code)
+        .filter((item, pos, groupNames) => groupNames.indexOf(item) === pos);
+    return series
+}
+
+// // a function to work out the extent of values in an array accross multiple properties...
+// export function extentMulti(data, columns) {
+//     const ext = data.reduce((acc, row) => {
+//         const values = columns.map(key => +row[key]);
+//         const rowExtent = d3.extent(values);
+//         if (!acc.max) {
+//             acc.max = rowExtent[1];
+//             acc.min = rowExtent[0];
+//         } else {
+//             acc.max = Math.max(acc.max, rowExtent[1]);
+//             acc.min = Math.min(acc.min, rowExtent[0]);
+//         }
+//         return acc;
+//     }, {});
+//     return [ext.min, ext.max];
+// }
---
+++ small-multiple-pie-donut-vacine/pieChart.js
@@ -0,0 +1,164 @@
+import * as d3 from 'd3';
+import * as gChartcolour from 'g-chartcolour';
+import 'd3-selection-multi';
+
+export function draw() {
+    let rem = 10;
+    const colourScale = d3.scaleOrdinal();
+    // .domain(['group']);
+    let colourProperty = 'group'; // eslint-disable-line no-unused-vars
+    let seriesNames = []; // eslint-disable-line no-unused-vars
+    let radius;
+    let smallRadius;
+    let totalSize;
+    let frameName;
+    let frameTimes;
+    let pieDim;
+    let plotDim;
+    let setHighlight;
+
+
+    function chart(parent) {
+        var pie = d3.pie()
+            .sort(null)
+            .value(function(d) { return d; });
+        var data = [1, 99]
+        console.log ("frameTimes", frameTimes)
+
+        parent.selectAll('.pie-name')
+        .data((d) => {
+            
+            console.log('data', d.chartData)
+            console.log('frameTimes', frameTimes)
+            const test = d.chartData.filter((el) => {el.date == frameTimes[0]})
+            console.log('test', test)
+            return d.chartData
+        })
+        .enter()
+        .append('text')
+        .attr('class','pie-name')
+        .attr('x',0)
+        .attr('dy', rem *1.5)
+        .text(d => d.area)
+        
+        var arc = d3.arc()
+            .outerRadius(50)
+            .innerRadius(10);
+
+        const smallPie = parent
+            .append ('g')
+            .selectAll(".arc")
+                .data(pie(data))
+                .enter()
+                .append("g")
+                .attr("class", "arc")
+                .append("path")
+                .attr("d", arc)
+                .style("fill", "#a3b2f1");
+        smallPie.attr('transform', `translate(${pieDim[0] * .5}, ${pieDim[1]})`)
+
+        
+            // parent.append ('g')
+            //     //.attr("transform", "translate(100,100);
+            // .selectAll('mySlices')
+            //     .data(data_ready)
+            //     .enter()
+            //     .append('path')
+            //         .attr('d', arcGenerator)
+            //         .attr('fill', '#fff3a4')
+            //         .attr("stroke", "black")
+            //         .style("stroke-width", "2px")
+            //         .style("opacity", 0.7);
+    }
+
+    chart.seriesNames = (d) => {
+        if (typeof d === 'undefined') return seriesNames;
+        seriesNames = d;
+        return chart;
+    };
+
+    chart.frameTimes = (d) => {
+        if (typeof d === 'undefined') return frameTimes;
+        frameTimes = d;
+        return chart;
+    };
+
+    chart.radius = (d) => {
+        if (!d) return radius;
+        radius = d;
+        return chart;
+    };
+
+    chart.smallRadius = (d) => {
+        if (!d) return radius;
+        smallRadius = d;
+        return chart;
+    };
+
+    chart.totalSize = (d) => {
+        if (!d) return totalSize;
+        totalSize = d;
+        return chart;
+    };
+
+    chart.colourPalette = (d) => {
+        if (!d) return colourScale;
+        if (d === 'social' || d === 'video') {
+            colourScale.range(gChartcolour.lineSocial);
+        } else if (['webS', 'webM', 'webMDefault', 'webL'].includes(d)) {
+            colourScale.range(gChartcolour.categorical_bar);
+        } else if (d === 'print') {
+            colourScale.range(gChartcolour.linePrint);
+        }
+        return chart;
+    };
+
+    chart.colourPicker = (d) => {
+        if (d === 'social' || d === 'video' || d === 'print') {
+            setHighlight = 1;
+        } else if (['webS', 'webM', 'webMDefault', 'webL'].includes(d)) {
+            setHighlight = 4;
+        }
+        return chart;
+    };
+
+    chart.colourRange = (x) => {
+        colourScale.range(x);
+        return chart;
+    };
+
+    chart.colourDomain = (x) => {
+        colourScale.domain(x);
+        return chart;
+    };
+
+    chart.colourProperty = (x) => {
+        colourProperty = x;
+        return chart;
+    };
+
+    chart.plotDim = (d) => {
+        if (!d) return window.plotDim;
+        window.plotDim = d;
+        return chart;
+    };
+
+    chart.pieDim = (d) => {
+        if (!d) return pieDim;
+        pieDim = d;
+        return chart;
+    };
+
+    chart.rem = (d) => {
+        if (!d) return rem;
+        rem = d;
+        return chart;
+    };
+    chart.frameName = (d) => {
+        if (!d) return frameName;
+        frameName = d;
+        return chart;
+    };
+
+    return chart;
+}
