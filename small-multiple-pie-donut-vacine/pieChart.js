0b9eb975a61b655e99b4676b0ef27e322c745d9e
Ændrew Rininsland <andrew.rininsland@ft.com>
2021-01-08 16:50:33 +0000

    Fixes labels of vaccine pie small multi

--- small-multiple-pie-donut-vacine/pieChart.js
+++ small-multiple-pie-donut-vacine/pieChart.js
@@ -1,12 +1,12 @@
-import * as d3 from 'd3';
-import * as gChartcolour from 'g-chartcolour';
-import 'd3-selection-multi';
+import * as d3 from "d3";
+import * as gChartcolour from "g-chartcolour";
+import "d3-selection-multi";
 
 export function draw() {
     let rem = 10;
     const colourScale = d3.scaleOrdinal();
     // .domain(['group']);
-    let colourProperty = 'group'; // eslint-disable-line no-unused-vars
+    let colourProperty = "group"; // eslint-disable-line no-unused-vars
     let seriesNames = []; // eslint-disable-line no-unused-vars
     let radius;
     let smallRadius;
@@ -17,68 +17,61 @@ export function draw() {
     let plotDim;
     let setHighlight;
 
-
     function chart(parent) {
-        var pie = d3.pie()
+        var pie = d3
+            .pie()
             .sort(null)
-            .value(function(d) { return d; });
-        var data = [1, 99]
-        console.log ("frameTimes", frameTimes)
+            .value(function (d) {
+                return d;
+            });
+        var data = [1, 99];
+        console.log("frameTimes", frameTimes);
 
-        parent.selectAll('.pie-name')
-        .data((d) => {
-            
-            console.log('data', d.chartData)
-            console.log('frameTimes', frameTimes)
-            const test = d.chartData.filter((el) => {el.date == frameTimes[0]})
-            console.log('test', test)
-            return d.chartData
-        })
-        .enter()
-        .append('text')
-        .attr('class','pie-name')
-        .attr('x',0)
-        .attr('dy', rem *1.5)
-        .text(d => d.area)
-        
-        var arc = d3.arc()
-            .outerRadius(50)
-            .innerRadius(10);
+        parent
+            .append("text")
+            .attr("class", "pie-name")
+            .attr("x", 0)
+            .attr("dy", rem * 1.5)
+            .text((d) => d.code);
+
+        var arc = d3.arc().outerRadius(50).innerRadius(10);
 
         const smallPie = parent
-            .append ('g')
+            .append("g")
             .selectAll(".arc")
-                .data(pie(data))
-                .enter()
-                .append("g")
-                .attr("class", "arc")
-                .append("path")
-                .attr("d", arc)
-                .style("fill", "#a3b2f1");
-        smallPie.attr('transform', `translate(${pieDim[0] * .5}, ${pieDim[1]})`)
+            .data(pie(data))
+            .enter()
+            .append("g")
+            .attr("class", "arc")
+            .append("path")
+            .attr("d", arc)
+            .style("fill", "#a3b2f1");
+        smallPie.attr(
+            "transform",
+            `translate(${pieDim[0] * 0.5}, ${pieDim[1]})`
+        );
 
-        
-            // parent.append ('g')
-            //     //.attr("transform", "translate(100,100);
-            // .selectAll('mySlices')
-            //     .data(data_ready)
-            //     .enter()
-            //     .append('path')
-            //         .attr('d', arcGenerator)
-            //         .attr('fill', '#fff3a4')
-            //         .attr("stroke", "black")
-            //         .style("stroke-width", "2px")
-            //         .style("opacity", 0.7);
+        // parent.append ('g')
+        //     //.attr("transform", "translate(100,100);
+        // .selectAll('mySlices')
+        //     .data(data_ready)
+        //     .enter()
+        //     .append('path')
+        //         .attr('d', arcGenerator)
+        //         .attr('fill', '#fff3a4')
+        //         .attr("stroke", "black")
+        //         .style("stroke-width", "2px")
+        //         .style("opacity", 0.7);
     }
 
     chart.seriesNames = (d) => {
-        if (typeof d === 'undefined') return seriesNames;
+        if (typeof d === "undefined") return seriesNames;
         seriesNames = d;
         return chart;
     };
 
     chart.frameTimes = (d) => {
-        if (typeof d === 'undefined') return frameTimes;
+        if (typeof d === "undefined") return frameTimes;
         frameTimes = d;
         return chart;
     };
@@ -103,20 +96,20 @@ export function draw() {
 
     chart.colourPalette = (d) => {
         if (!d) return colourScale;
-        if (d === 'social' || d === 'video') {
+        if (d === "social" || d === "video") {
             colourScale.range(gChartcolour.lineSocial);
-        } else if (['webS', 'webM', 'webMDefault', 'webL'].includes(d)) {
+        } else if (["webS", "webM", "webMDefault", "webL"].includes(d)) {
             colourScale.range(gChartcolour.categorical_bar);
-        } else if (d === 'print') {
+        } else if (d === "print") {
             colourScale.range(gChartcolour.linePrint);
         }
         return chart;
     };
 
     chart.colourPicker = (d) => {
-        if (d === 'social' || d === 'video' || d === 'print') {
+        if (d === "social" || d === "video" || d === "print") {
             setHighlight = 1;
-        } else if (['webS', 'webM', 'webMDefault', 'webL'].includes(d)) {
+        } else if (["webS", "webM", "webMDefault", "webL"].includes(d)) {
             setHighlight = 4;
         }
         return chart;
